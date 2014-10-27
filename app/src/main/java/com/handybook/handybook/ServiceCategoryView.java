1a2b76ad3aa27e50db7ec8fe2b92375349e210e4
Webster Ross III <websterross@handybook.com>
2014-10-27 13:33:52 -0400

    Updated styling for main screen

--- app/src/main/java/com/handybook/handybook/ServiceCategoryView.java
+++ app/src/main/java/com/handybook/handybook/ServiceCategoryView.java
@@ -27,7 +27,6 @@ final class ServiceCategoryView extends FrameLayout {
     private void init(final Context context) {
         LayoutInflater.from(context).inflate(R.layout.view_service_category, this);
         textView = (TextView)this.findViewById(R.id.text_view);
-        textView.setTextColor(getResources().getColor(R.color.handy_blue));
     }
 
     final void setText(final String text) {
