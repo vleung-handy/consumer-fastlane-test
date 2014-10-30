5993fb38a9e4ea8e61d77482f706ca7e99f5fe37
Webster Ross III <websterross@handybook.com>
2014-10-30 10:14:10 -0400

    Fixed new relic issue

--- app/src/main/java/com/handybook/handybook/BaseActivity.java
+++ app/src/main/java/com/handybook/handybook/BaseActivity.java
@@ -14,10 +14,19 @@ abstract class BaseActivity extends FragmentActivity {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        NewRelic.withApplicationToken("AAbaf8c55fb9788d1664e82661d94bc18ea7c39aa6")
-                .start(this.getApplication());
 
-        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        if (BuildConfig.FLAVOR.equals(BaseApplication.FLAVOR_PROD)) {
+            NewRelic.withApplicationToken("AAbaf8c55fb9788d1664e82661d94bc18ea7c39aa6")
+                    .start(this.getApplication());
+        }
+        else {
+            NewRelic.withApplicationToken("AAbaf8c55fb9788d1664e82661d94bc18ea7c39aa6")
+                    .start(this.getApplication());
+        }
+
+        if (!BuildConfig.FLAVOR.equals(BaseApplication.FLAVOR_STAGE) && !BuildConfig.BUILD_TYPE.equals("debug")) {
+            setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
     }
 
     @Override
