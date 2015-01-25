e8ad9800329b8b18de77940256c76717486c9409
Webster Ross III <websterross@handybook.com>
2015-01-25 17:00:29 -0500

    Implement web help button

--- app/src/main/java/com/handybook/handybook/ui/fragment/HelpFragment.java
+++ app/src/main/java/com/handybook/handybook/ui/fragment/HelpFragment.java
@@ -1,9 +1,12 @@
 package com.handybook.handybook.ui.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.handybook.handybook.R;
 import com.handybook.handybook.ui.widget.MenuButton;
@@ -13,6 +16,8 @@ import butterknife.InjectView;
 
 public final class HelpFragment extends InjectedFragment {
     @InjectView(R.id.menu_button_layout) ViewGroup menuButtonLayout;
+    @InjectView(R.id.web_button) Button webButton;
+    @InjectView(R.id.phone_button) Button phoneButton;
 
     public static HelpFragment newInstance() {
         return new HelpFragment();
@@ -28,6 +33,16 @@ public final class HelpFragment extends InjectedFragment {
 
         final MenuButton menuButton = new MenuButton(getActivity(), menuButtonLayout);
         menuButtonLayout.addView(menuButton);
+
+        webButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://"
+                        + getString(R.string.web_help)));
+
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
