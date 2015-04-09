368ef422c00bcdf116385bcb8453e90dcd1465bc
Webster Ross III <websterross@handybook.com>
2015-04-09 15:42:02 -0400

    Add deeplink for reschedule booking

---
+++ app/src/main/java/com/handybook/handybook/ui/activity/SplashActivity.java
@@ -0,0 +1,126 @@
+package com.handybook.handybook.ui.activity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.handybook.handybook.R;
+import com.handybook.handybook.core.Booking;
+import com.handybook.handybook.core.User;
+import com.handybook.handybook.data.DataManager;
+
+import butterknife.ButterKnife;
+
+public class SplashActivity extends BaseActivity {
+    private static final String STATE_LAUNCHED_NEXT = "LAUNCHED_NEXT";
+
+    private User user;
+    private boolean launchedNext;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.inject(this);
+
+        if (savedInstanceState != null) {
+            launchedNext = savedInstanceState.getBoolean(STATE_LAUNCHED_NEXT, false);
+        }
+
+        if (!launchedNext) {
+            user = userManager.getCurrentUser();
+
+            final Intent intent = this.getIntent();
+            final String action = intent.getAction();
+            final Uri data = intent.getData();
+
+            if (!action.equals("android.intent.action.VIEW") || !data.getScheme().equals("handy")) {
+                openServiceCategoriesActivity();
+                return;
+            }
+
+            final String host = data.getHost();
+            final String path = data.getPath();
+
+            switch (host + path) {
+                case "booking/reschedule":
+                    openRescheduleActivity(data.getQueryParameter("booking_id"));
+                    break;
+
+                default:
+                    openServiceCategoriesActivity();
+            }
+        }
+        else openServiceCategoriesActivity();
+    }
+
+    @Override
+    public void startActivity(final Intent intent) {
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK
+                | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        super.startActivity(intent);
+
+        launchedNext = true;
+        finish();
+    }
+
+    @Override
+    public void startActivityForResult(final Intent intent, final int resultCode) {
+        super.startActivityForResult(intent, resultCode);
+        launchedNext = true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == BookingDateActivity.RESULT_RESCHEDULE_NEW_DATE) {
+            openServiceCategoriesActivity();
+        }
+    }
+
+    @Override
+    public final void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(STATE_LAUNCHED_NEXT, launchedNext);
+    }
+
+    private void openServiceCategoriesActivity() {
+        startActivity(new Intent(this, ServiceCategoriesActivity.class));
+    }
+
+    private void openRescheduleActivity(final String bookingId) {
+        dataManager.getBooking(bookingId, user != null ? user.getAuthToken() : null,
+                new DataManager.Callback<Booking>() {
+                    @Override
+                    public void onSuccess(final Booking booking) {
+                        if (!allowCallbacks) return;
+
+                        dataManager.getPreRescheduleInfo(bookingId, new DataManager.Callback<String>() {
+                            @Override
+                            public void onSuccess(final String notice) {
+                                if (!allowCallbacks) return;
+
+                                final Intent intent = new Intent(SplashActivity.this, BookingDateActivity.class);
+                                intent.putExtra(BookingDateActivity.EXTRA_RESCHEDULE_BOOKING, booking);
+                                intent.putExtra(BookingDateActivity.EXTRA_RESCHEDULE_NOTICE, notice);
+                                startActivityForResult(intent, BookingDateActivity.RESULT_RESCHEDULE_NEW_DATE);
+                            }
+
+                            @Override
+                            public void onError(final DataManager.DataManagerError error) {
+                                if (!allowCallbacks) return;
+                                dataManagerErrorHandler.handleError(SplashActivity.this, error);
+                                openServiceCategoriesActivity();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onError(final DataManager.DataManagerError error) {
+                        if (!allowCallbacks) return;
+                        dataManagerErrorHandler.handleError(SplashActivity.this, error);
+                        openServiceCategoriesActivity();
+                    }
+                });
+    }
+}
