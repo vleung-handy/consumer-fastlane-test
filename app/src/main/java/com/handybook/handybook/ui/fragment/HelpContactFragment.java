6d602ffde6880336f2892ab2d2db57dbe2e64894
cdavis-handy <cdavis@handy.com>
2015-04-17 11:33:29 -0400

    added exit button to help contact form

--- app/src/main/java/com/handybook/handybook/ui/fragment/HelpContactFragment.java
+++ app/src/main/java/com/handybook/handybook/ui/fragment/HelpContactFragment.java
@@ -3,6 +3,7 @@ package com.handybook.handybook.ui.fragment;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
@@ -28,6 +29,7 @@ import com.handybook.handybook.ui.widget.FirstNameInputTextView;
 import com.handybook.handybook.ui.widget.InputTextField;
 import com.handybook.handybook.ui.widget.MenuButton;
 import com.handybook.handybook.util.TextUtils;
+import com.simplealertdialog.SimpleAlertDialog;
 
 import org.json.JSONObject;
 
@@ -52,10 +54,16 @@ public final class HelpContactFragment extends InjectedFragment {
     private static final String HELP_CONTACT_FORM_DESCRIPTION = "description";
     private static final String SALESFORCE_DATA_WRAPPER_KEY = "salesforce_data";
 
-    @InjectView(R.id.send_message_button) Button sendMessageButton;
-    @InjectView(R.id.user_name_text) FirstNameInputTextView nameText;
-    @InjectView(R.id.email_text) EmailInputTextView emailText;
-    @InjectView(R.id.comment_text) BasicInputTextView commentText;
+    @InjectView(R.id.send_message_button)
+    Button sendMessageButton;
+    @InjectView(R.id.user_name_text)
+    FirstNameInputTextView nameText;
+    @InjectView(R.id.email_text)
+    EmailInputTextView emailText;
+    @InjectView(R.id.comment_text)
+    BasicInputTextView commentText;
+    @InjectView(R.id.close_img)
+    ImageView closeImage;
 
     private HelpNode associatedNode;
 
@@ -83,62 +91,70 @@ public final class HelpContactFragment extends InjectedFragment {
 
         sendMessageButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                Boolean allValid = true;
-                allValid &= nameText.validate();
-                allValid &= emailText.validate();
-                allValid &= commentText.validate();
-
-                if(allValid)
-                {
-                    progressDialog.show();
-
-                    //Generates params from the Help nodes
-                    HashMap<String, String> contactFormInfo = parseHelpNode(associatedNode);
-
-                    //add contact form information
-                    contactFormInfo.put(HELP_CONTACT_FORM_NAME, nameText.getText().toString());
-                    contactFormInfo.put(HELP_CONTACT_FORM_EMAIL, emailText.getText().toString());
-                    contactFormInfo.put(HELP_CONTACT_FORM_DESCRIPTION, commentText.getText().toString());
-
-                    JSONObject salesforceWrapper =  new JSONObject();
-                    try {
-                        salesforceWrapper.put(SALESFORCE_DATA_WRAPPER_KEY, new JSONObject(contactFormInfo));
-                    }
-                    catch (Exception e)
-                    {}
-
-                    TypedInput body;
-                    try {
-                        body = new TypedByteArray("application/json", salesforceWrapper.toString().getBytes("UTF-8"));
-                    }
-                    catch (UnsupportedEncodingException e) {
-                        body = null;
-                    }
-                    dataManager.createHelpCase(body, createCaseCallback);
-                }
+            public void onClick(View v) {
+                onSendMessageButtonClick();
             }
         });
+
+        closeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCloseImageClick();
+            }
+        });
+
 
         return view;
     }
 
-    private HashMap<String, String> parseHelpNode(HelpNode node)
-    {
+    private void onSendMessageButtonClick() {
+        Boolean allValid = true;
+        allValid &= nameText.validate();
+        allValid &= emailText.validate();
+        allValid &= commentText.validate();
+
+        if (allValid) {
+            progressDialog.show();
+
+            //Generates params from the Help nodes
+            HashMap<String, String> contactFormInfo = parseHelpNode(associatedNode);
+
+            //add contact form information
+            contactFormInfo.put(HELP_CONTACT_FORM_NAME, nameText.getText().toString());
+            contactFormInfo.put(HELP_CONTACT_FORM_EMAIL, emailText.getText().toString());
+            contactFormInfo.put(HELP_CONTACT_FORM_DESCRIPTION, commentText.getText().toString());
+
+            JSONObject salesforceWrapper = new JSONObject();
+            try {
+                salesforceWrapper.put(SALESFORCE_DATA_WRAPPER_KEY, new JSONObject(contactFormInfo));
+            } catch (Exception e) {
+            }
+
+            TypedInput body;
+            try {
+                body = new TypedByteArray("application/json", salesforceWrapper.toString().getBytes("UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                body = null;
+            }
+            dataManager.createHelpCase(body, createCaseCallback);
+        }
+    }
+
+    private void onCloseImageClick() {
+        returnToHomeScreen();
+    }
+
+    private HashMap<String, String> parseHelpNode(HelpNode node) {
         HashMap<String, String> params = new HashMap<String, String>();
-        for(HelpNode childNode : node.getChildren())
-        {
-            if(childNode.getType().equals(HELP_CONTACT_FORM_DISPOSITION))
-            {
+        for (HelpNode childNode : node.getChildren()) {
+            if (childNode.getType().equals(HELP_CONTACT_FORM_DISPOSITION)) {
                 params.put(childNode.getLabel(), childNode.getContent());
             }
         }
         return params;
     }
 
-    private void returnToHelpScreenStart()
-    {
+    private void returnToHomeScreen() {
         final Intent toHomeScreenIntent = new Intent(getActivity(), ServiceCategoriesActivity.class);
         toHomeScreenIntent.addFlags((Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
         startActivity(toHomeScreenIntent);
@@ -149,7 +165,7 @@ public final class HelpContactFragment extends InjectedFragment {
         public void onSuccess(final Void v) {
             if (!allowCallbacks) return;
             progressDialog.dismiss();
-            returnToHelpScreenStart();
+            returnToHomeScreen();
         }
 
         @Override
