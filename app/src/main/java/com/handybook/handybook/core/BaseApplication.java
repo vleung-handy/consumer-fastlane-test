edc981be0b42be6162a2c606464d81128943374f
Ondrej Dolejsi <odolejsi@handy.com>
2015-09-23 10:32:11 -0400

    BaseApllication registers ActivityLifecycleCallbacks that send an event for each one of them.

--- app/src/main/java/com/handybook/handybook/core/BaseApplication.java
+++ app/src/main/java/com/handybook/handybook/core/BaseApplication.java
@@ -2,21 +2,20 @@ package com.handybook.handybook.core;
 
 import android.app.Activity;
 import android.app.Application;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.crashlytics.android.Crashlytics;
 import com.handybook.handybook.BuildConfig;
 import com.handybook.handybook.R;
-import com.handybook.handybook.constant.PrefsKey;
 import com.handybook.handybook.data.DataManager;
 import com.handybook.handybook.data.Mixpanel;
+import com.handybook.handybook.event.ActivityEvent;
+import com.handybook.handybook.manager.AppBlockManager;
 import com.handybook.handybook.manager.HelpContactManager;
 import com.handybook.handybook.manager.HelpManager;
 import com.handybook.handybook.manager.PrefsManager;
-import com.handybook.handybook.ui.activity.BlockingActivity;
 import com.newrelic.agent.android.NewRelic;
+import com.squareup.otto.Bus;
 import com.urbanairship.AirshipConfigOptions;
 import com.urbanairship.UAirship;
 import com.urbanairship.push.notifications.DefaultNotificationFactory;
@@ -31,7 +30,6 @@ public class BaseApplication extends Application
 {
     public static final String FLAVOR_PROD = "prod";
     public static final String FLAVOR_STAGE = "stage";
-    public static final long MIN_APP_BLOCKED_CHECK_INTERVAL = 30 * 1000; // No more than every 30s
 
     protected ObjectGraph graph;
     private int started;
@@ -43,15 +41,20 @@ public class BaseApplication extends Application
     DataManager dataManager;
     @Inject
     Mixpanel mixpanel;
+    @Inject
+    Bus bus;
 
 
-    //We are injecting all of our event bus listening managers in BaseApplication to start them up for event listening
+    //We are injecting all of our event bus listening managers in BaseApplication to start them
+    // up for event listening
     @Inject
     HelpManager helpManager;
     @Inject
     HelpContactManager helpContactManager;
     @Inject
     PrefsManager prefsManager;
+    @Inject
+    AppBlockManager appBlockManager;
 
 
     @Override
@@ -100,14 +103,15 @@ public class BaseApplication extends Application
             public void onActivityCreated(final Activity activity,
                     final Bundle savedInstanceState)
             {
+                bus.post(new ActivityEvent.Created(activity, savedInstanceState));
                 savedInstance = savedInstanceState != null;
             }
 
             @Override
             public void onActivityStarted(final Activity activity)
             {
+                bus.post(new ActivityEvent.Started(activity));
                 ++started;
-
                 if (started == 1)
                 {
                     if (!savedInstance)
@@ -124,115 +128,36 @@ public class BaseApplication extends Application
             @Override
             public void onActivityResumed(final Activity activity)
             {
-                if (shoulBlockActivity(activity))
-                {
-                    activity.finish();
-                    showBlockingScreen();
-                }
+                bus.post(new ActivityEvent.Resumed(activity));
             }
 
             @Override
             public void onActivityPaused(final Activity activity)
             {
+                bus.post(new ActivityEvent.Paused(activity));
             }
 
             @Override
             public void onActivityStopped(final Activity activity)
             {
+                bus.post(new ActivityEvent.Stopped(activity));
             }
 
             @Override
-            public void onActivitySaveInstanceState(final Activity activity,
+            public void onActivitySaveInstanceState(
+                    final Activity activity,
                     final Bundle outState)
             {
+                bus.post(new ActivityEvent.SavedInstanceState(activity, outState));
             }
 
             @Override
             public void onActivityDestroyed(final Activity activity)
             {
+                bus.post(new ActivityEvent.Destroyed(activity));
             }
         });
     }
-
-    private boolean shoulBlockActivity(final Activity activity)
-    {
-        // If we are resuming an activity other than blocking activity and app is blocked
-        return isAppBlocked() && !activity.getClass().equals(BlockingActivity.class);
-    }
-
-    private void showBlockingScreen()
-    {
-        Intent launchBlockingActivity = new Intent(this, BlockingActivity.class);
-        launchBlockingActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        launchBlockingActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(launchBlockingActivity);
-    }
-
-    private boolean isAppBlocked()
-    {
-        if (shouldUpdateBlockingStateFromApi())
-        {
-            updateBlockingStateFromApi();
-        }
-        return prefsManager.getBoolean(PrefsKey.APP_BLOCKED, false);
-    }
-
-    private boolean shouldUpdateBlockingStateFromApi()
-    {
-        final long lastBlockedCheckMillis = prefsManager.getLong(PrefsKey.APP_BLOCKED_LAST_CHECK, 0);
-        return System.currentTimeMillis() - lastBlockedCheckMillis > MIN_APP_BLOCKED_CHECK_INTERVAL;
-    }
-
-    /**
-     * Request ShouldBlock object from API and update shared preferences accordingly
-     */
-    private void updateBlockingStateFromApi()
-    {
-        int versionCode;
-        try
-        {
-            versionCode = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
-        } catch (PackageManager.NameNotFoundException nnfe)
-        {
-            nnfe.printStackTrace();
-            Crashlytics.logException(nnfe);
-            versionCode = 0;
-        }
-        dataManager.getShouldBlockObject(
-                versionCode,
-                new DataManager.CacheResponse<ShouldBlockObject>()
-                {
-                    @Override
-                    public void onResponse(final ShouldBlockObject shouldBlockObject)
-                    {
-                        //Do nothing, what is this even for?
-                    }
-                },
-                new DataManager.Callback<ShouldBlockObject>()
-                {
-                    @Override
-                    public void onSuccess(ShouldBlockObject response)
-                    {
-                        updateAppBlockedSharedPreference(response.isBlocked());
-                    }
-
-                    @Override
-                    public void onError(DataManager.DataManagerError error)
-                    {
-                        Crashlytics.log("Get should block object error.");
-                        // Otherwise do nothing. We default to letting people use the app, so in
-                        // case of request error we stick to that policy.
-                    }
-                });
-
-    }
-
-    private void updateAppBlockedSharedPreference(final boolean isBlocked)
-    {
-        prefsManager.setLong(PrefsKey.APP_BLOCKED_LAST_CHECK, System.currentTimeMillis());
-        prefsManager.setBoolean(PrefsKey.APP_BLOCKED, isBlocked);
-    }
-
 
     public final void inject(final Object object)
     {
