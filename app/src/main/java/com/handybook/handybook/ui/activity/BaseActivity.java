cac35b18f5b4d5a91546422d76e5aa7708603bf1
Ondrej Dolejsi <odolejsi@handy.com>
2015-10-07 14:14:00 -0400

    Bookings page now has two tabs with two instances of BookingListFragment. Tabs are clickable, ViewPager is swipable. BookingListFragment uses SwipeRefreshLayout to allow user to request updated list of bookings from the API.

--- app/src/main/java/com/handybook/handybook/ui/activity/BaseActivity.java
+++ app/src/main/java/com/handybook/handybook/ui/activity/BaseActivity.java
@@ -7,8 +7,8 @@ import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.widget.Toast;
 
@@ -35,172 +35,220 @@ import javax.inject.Inject;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public abstract class BaseActivity extends FragmentActivity {
+public abstract class BaseActivity extends AppCompatActivity
+{
 
 
     protected boolean allowCallbacks;
-    private OnBackPressedListener onBackPressedListener;
-    private RateServiceDialogFragment rateServiceDialog;
     protected ProgressDialog progressDialog;
     protected Toast toast;
+    @Inject
+    Mixpanel mixpanel;
+    @Inject
+    UserManager userManager;
+    @Inject
+    DataManager dataManager;
+    @Inject
+    DataManagerErrorHandler dataManagerErrorHandler;
+    @Inject
+    NavigationManager navigationManager;
+    @Inject
+    Bus bus;
+    private OnBackPressedListener onBackPressedListener;
+    private RateServiceDialogFragment rateServiceDialog;
 
     //Public Properties
-    public boolean getAllowCallbacks() { return this.allowCallbacks; }
-
-    @Inject Mixpanel mixpanel;
-    @Inject UserManager userManager;
-    @Inject DataManager dataManager;
-    @Inject DataManagerErrorHandler dataManagerErrorHandler;
-    @Inject NavigationManager navigationManager;
-    @Inject Bus bus;
+    public boolean getAllowCallbacks()
+    {
+        return this.allowCallbacks;
+    }
 
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-
         Yozio.initialize(this);
-
-        ((BaseApplication)this.getApplication()).inject(this);
-
+        ((BaseApplication) this.getApplication()).inject(this);
         if (!BuildConfig.FLAVOR.equals(BaseApplication.FLAVOR_STAGE)
-                && !BuildConfig.BUILD_TYPE.equals("debug")) {
-            setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                && !BuildConfig.BUILD_TYPE.equals("debug"))
+        {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
             Yozio.YOZIO_ENABLE_LOGGING = false;
         }
-
         final Intent intent = getIntent();
         final Uri data = intent.getData();
-
-        if (data != null && data.getHost() != null && data.getHost().equals("deeplink.yoz.io")) {
+        if (data != null && data.getHost() != null && data.getHost().equals("deeplink.yoz.io"))
+        {
             mixpanel.trackEventYozioOpen(Yozio.getMetaData(intent));
         }
-
         toast = Toast.makeText(this, null, Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER, 0, 0);
-
         progressDialog = new ProgressDialog(this);
         progressDialog.setDelay(400);
         progressDialog.setCancelable(false);
         progressDialog.setMessage(getString(R.string.loading));
     }
 
-    //Lifecycle
     @Override
-    protected void onStart() {
-        super.onStart();
-        if (PlayServicesUtils.isGooglePlayStoreAvailable()) {
-            PlayServicesUtils.handleAnyPlayServicesError(this);
-        }
-        allowCallbacks = true;
-    }
-
-    @Override
-    protected void onStop() {
+    protected void onStop()
+    {
         super.onStop();
         allowCallbacks = false;
     }
 
     @Override
-    protected void onPostResume() {
+    protected void onPostResume()
+    {
         super.onPostResume();
         showRequiredUserModals();
     }
 
     @Override
-    protected final void attachBaseContext(final Context newBase) {
-        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (onBackPressedListener != null) onBackPressedListener.onBack();
-        else super.onBackPressed();
-    }
-
-    @Override
-    protected void onDestroy() {
+    protected void onDestroy()
+    {
         mixpanel.flush();
         super.onDestroy();
     }
 
-    public void setOnBackPressedListener(final OnBackPressedListener onBackPressedListener) {
-        this.onBackPressedListener = onBackPressedListener;
-    }
-
-    public interface OnBackPressedListener {
-        void onBack();
-    }
-
-    private void showRequiredUserModals() {
+    private void showRequiredUserModals()
+    {
         final FragmentManager fm = getSupportFragmentManager();
         final User user = userManager.getCurrentUser();
-
         if (user == null || fm.findFragmentByTag("RateServiceDialogFragment") != null
                 || fm.findFragmentByTag("LaundryDropOffDialogFragment") != null
                 || fm.findFragmentByTag("LaundryInfoDialogFragment") != null
-                || !(BaseActivity.this instanceof ServiceCategoriesActivity)) return;
-
-        dataManager.getUser(user.getId(), user.getAuthToken(), new DataManager.Callback<User>() {
+                || !(BaseActivity.this instanceof ServiceCategoriesActivity))
+        {
+            return;
+        }
+        dataManager.getUser(user.getId(), user.getAuthToken(), new DataManager.Callback<User>()
+        {
             @Override
-            public void onSuccess(final User user) {
-                if (!allowCallbacks) return;
-
+            public void onSuccess(final User user)
+            {
+                if (!allowCallbacks)
+                {
+                    return;
+                }
                 final int laundryBookingId = user.getLaundryBookingId();
                 final int addLaundryBookingId = user.getAddLaundryBookingId();
                 final String proName = user.getBookingRatePro();
-                final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(BaseActivity.this);
-
-                if (addLaundryBookingId > 0 && !prefs.getBoolean("APP_LAUNDRY_INFO_SHOWN", false)) {
+                final SharedPreferences prefs = PreferenceManager
+                        .getDefaultSharedPreferences(BaseActivity.this);
+                if (addLaundryBookingId > 0 && !prefs.getBoolean("APP_LAUNDRY_INFO_SHOWN", false))
+                {
                     showLaundryInfoModal(addLaundryBookingId, user.getAuthToken());
-                }
-                else if (laundryBookingId > 0) showLaundryDropOffModal(laundryBookingId, user.getAuthToken());
-                else if (proName != null) {
+                } else if (laundryBookingId > 0)
+                {
+                    showLaundryDropOffModal(
+                            laundryBookingId,
+                            user.getAuthToken()
+                    );
+                } else if (proName != null)
+                {
                     final int bookingId = user.getBookingRateId();
-
                     rateServiceDialog = RateServiceDialogFragment
                             .newInstance(bookingId, proName, -1);
-
                     rateServiceDialog.show(BaseActivity.this.getSupportFragmentManager(),
                             "RateServiceDialogFragment");
-
                     mixpanel.trackEventProRate(Mixpanel.ProRateEventType.SHOW, bookingId,
                             proName, 0);
                 }
             }
 
             @Override
-            public void onError(DataManager.DataManagerError error) {}
+            public void onError(DataManager.DataManagerError error)
+            {
+            }
         });
     }
 
-    private void showLaundryDropOffModal(final int bookingId, final String authToken) {
+    private void showLaundryInfoModal(final int bookingId, final String authToken)
+    {
+        dataManager.getAddLaundryInfo(bookingId, authToken, new DataManager.Callback<Booking>()
+        {
+            @Override
+            public void onSuccess(final Booking booking)
+            {
+                if (!allowCallbacks)
+                {
+                    return;
+                }
+
+                LaundryInfoDialogFragment.newInstance(booking).show(
+                        BaseActivity.this.getSupportFragmentManager(),
+                        "LaundryInfoDialogFragment"
+                );
+            }
+
+            @Override
+            public void onError(final DataManager.DataManagerError error)
+            {
+            }
+        });
+    }
+
+    private void showLaundryDropOffModal(final int bookingId, final String authToken)
+    {
         dataManager.getLaundryScheduleInfo(bookingId, authToken,
-                new DataManager.Callback<LaundryDropInfo>() {
-            @Override
-            public void onSuccess(final LaundryDropInfo info) {
-                if (!allowCallbacks) return;
+                new DataManager.Callback<LaundryDropInfo>()
+                {
+                    @Override
+                    public void onSuccess(final LaundryDropInfo info)
+                    {
+                        if (!allowCallbacks)
+                        {
+                            return;
+                        }
 
-                LaundryDropOffDialogFragment.newInstance(bookingId, info)
-                        .show(BaseActivity.this.getSupportFragmentManager(), "LaundryDropOffDialogFragment");
-            }
+                        LaundryDropOffDialogFragment.newInstance(bookingId, info)
+                                .show(BaseActivity.this.getSupportFragmentManager(), "LaundryDropOffDialogFragment");
+                    }
 
-            @Override
-            public void onError(final DataManager.DataManagerError error) {}
-        });
+                    @Override
+                    public void onError(final DataManager.DataManagerError error)
+                    {
+                    }
+                });
     }
 
-    private void showLaundryInfoModal(final int bookingId, final String authToken) {
-        dataManager.getAddLaundryInfo(bookingId, authToken, new DataManager.Callback<Booking>() {
-            @Override
-            public void onSuccess(final Booking booking) {
-                if (!allowCallbacks) return;
+    @Override
+    protected final void attachBaseContext(final Context newBase)
+    {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
-                LaundryInfoDialogFragment.newInstance(booking)
-                        .show(BaseActivity.this.getSupportFragmentManager(), "LaundryInfoDialogFragment");
-            }
+    @Override
+    public void onBackPressed()
+    {
+        if (onBackPressedListener != null)
+        {
+            onBackPressedListener.onBack();
+        } else
+        {
+            super.onBackPressed();
+        }
+    }
 
-            @Override
-            public void onError(final DataManager.DataManagerError error) {}
-        });
+    //Lifecycle
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        if (PlayServicesUtils.isGooglePlayStoreAvailable())
+        {
+            PlayServicesUtils.handleAnyPlayServicesError(this);
+        }
+        allowCallbacks = true;
+    }
+
+    public void setOnBackPressedListener(final OnBackPressedListener onBackPressedListener)
+    {
+        this.onBackPressedListener = onBackPressedListener;
+    }
+
+    public interface OnBackPressedListener
+    {
+        void onBack();
     }
 }
