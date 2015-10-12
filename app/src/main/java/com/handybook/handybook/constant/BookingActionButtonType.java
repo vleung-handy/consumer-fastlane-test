06d6e190068970b483976e44a728a13d175800c7
Hayley Anderson <handerson@handy.com>
2015-10-12 15:33:25 -0400

    Add ids to booking detail view buttons
    * Cancel and reschedule tests need ids to access related buttons
    * Add accessibility ids to action buttons on booking detail view

--- app/src/main/java/com/handybook/handybook/constant/BookingActionButtonType.java
+++ app/src/main/java/com/handybook/handybook/constant/BookingActionButtonType.java
@@ -4,21 +4,23 @@ import com.handybook.handybook.R;
 
 public enum BookingActionButtonType
 {
-    RESCHEDULE(BookingAction.ACTION_RESCHEDULE, R.string.reschedule, BookingActionButtonStyle.BLUE),
-    CANCEL(BookingAction.ACTION_CANCEL, R.string.cancel_booking, BookingActionButtonStyle.GREY),
-    CONTACT_PHONE(BookingAction.ACTION_CONTACT_PHONE, R.string.call, BookingActionButtonStyle.CONTACT),
-    CONTACT_TEXT(BookingAction.ACTION_CONTACT_TEXT, R.string.text, BookingActionButtonStyle.CONTACT),
+    RESCHEDULE(BookingAction.ACTION_RESCHEDULE, R.string.reschedule, BookingActionButtonStyle.BLUE, R.id.action_button_reschedule_booking),
+    CANCEL(BookingAction.ACTION_CANCEL, R.string.cancel_booking, BookingActionButtonStyle.GREY, R.id.action_button_cancel_booking),
+    CONTACT_PHONE(BookingAction.ACTION_CONTACT_PHONE, R.string.call, BookingActionButtonStyle.CONTACT, R.id.action_button_contact_phone),
+    CONTACT_TEXT(BookingAction.ACTION_CONTACT_TEXT, R.string.text, BookingActionButtonStyle.CONTACT, R.id.action_button_contact_text),
     ;
 
     private String actionName; //TODO: Enum this?
     private int displayNameId;
     private BookingActionButtonStyle style;
+    private int accessibilityId;
 
-    BookingActionButtonType(String actionName, int displayNameId, BookingActionButtonStyle style)
+    BookingActionButtonType(String actionName, int displayNameId, BookingActionButtonStyle style, int accessibilityId)
     {
         this.actionName = actionName;
         this.displayNameId = displayNameId;
         this.style = style;
+        this.accessibilityId = accessibilityId;
     }
 
     public int getBackgroundDrawableId()
@@ -44,5 +46,9 @@ public enum BookingActionButtonType
     public int getTextStyleId()
     {
         return style.getTextStyleId();
+    }
+
+    public int getAccessibilityId() {
+        return accessibilityId;
     }
 }
