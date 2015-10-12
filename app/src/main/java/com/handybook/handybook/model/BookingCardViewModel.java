eb885286f3adf13045191aabc7964ff46334ef8f
Ondrej Dolejsi <odolejsi@handy.com>
2015-10-12 12:58:54 -0400

    BokingCardRowViewModel and BookingCardViewModel classes (not finished yet, but fleshed out)

--- app/src/main/java/com/handybook/handybook/model/BookingCardViewModel.java
+++ app/src/main/java/com/handybook/handybook/model/BookingCardViewModel.java
@@ -7,7 +7,9 @@ import com.handybook.handybook.core.Booking;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 
 public class BookingCardViewModel
 {
@@ -63,6 +65,44 @@ public class BookingCardViewModel
     public static class List extends ArrayList<BookingCardViewModel>
     {
 
+        public static List from(@NonNull final Collection<Booking> bookings)
+        {
+            final HashMap<Long, BookingCardViewModel> recurringIdToBCVM = new HashMap<>();
+            final List bookingCardViewModels = new List();
+            final Iterator<Booking> bookingIterator = bookings.iterator();
+            while (bookingIterator.hasNext())
+            {
+                Booking eachBooking = bookingIterator.next();
+                // If it's part of recurring booking
+                if (eachBooking.isRecurring())
+                {
+                    BookingCardViewModel bcvm = recurringIdToBCVM.get(
+                            eachBooking.getRecurringId()
+                    );
+                    if (bcvm == null)
+                    {
+                        // We haven't seen a booking from this recurring series
+                        bcvm = new BookingCardViewModel(eachBooking);
+                        recurringIdToBCVM.put(eachBooking.getRecurringId(), bcvm);
+                        bookingCardViewModels.add(bcvm);
+                    } else {
+                        // We have seen one from this series, add it to it's parent
+                        bcvm.addBooking(eachBooking);
+                    }
+                }
+                // If not part of recurring then just add to list
+                else
+                {
+                    bookingCardViewModels.add(new BookingCardViewModel(eachBooking));
+                }
+            }
+            return bookingCardViewModels;
+        }
+    }
+
+    private void addBooking(@NonNull final Booking booking)
+    {
+        mBookings.add(booking);
     }
 
 }
