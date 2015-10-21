829f4441917707b3ed41765e956317d61d743fc6
Victoria Leung <vleung@handybook.com>
2015-10-21 15:55:38 -0400

    revert edit frequency discount % logic due to change in business requirements

--- app/src/main/java/com/handybook/handybook/core/BookingPricesForFrequenciesResponse.java
+++ app/src/main/java/com/handybook/handybook/core/BookingPricesForFrequenciesResponse.java
@@ -1,59 +1,27 @@
 package com.handybook.handybook.core;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import com.google.gson.annotations.SerializedName;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class BookingPricesForFrequenciesResponse implements Parcelable //parcelable so that header fragment can store and then re-create this if it dies
+public class BookingPricesForFrequenciesResponse
 {
     //server returns these prices as dollars
-    @SerializedName("price_formatted")
-    private String priceFormatted;
     @SerializedName("weekly_price_formatted")
     private String weeklyPriceFormatted;
     @SerializedName("bimonthly_price_formatted")
     private String bimonthlyPriceFormatted;
     @SerializedName("monthly_price_formatted")
     private String monthlyPriceFormatted;
-
-
-    @SerializedName("discount_price_formatted")
-    private String discountPriceFormatted;
-    @SerializedName("weekly_discount_price_formatted")
-    private String weeklyDiscountPriceFormatted;
-    @SerializedName("bimonthly_discount_price_formatted")
-    private String bimonthlyDiscountPriceFormatted;
-    @SerializedName("monthly_discount_price_formatted")
-    private String monthlyDiscountPriceFormatted;
     @SerializedName("current_freq")
     private int currentFrequency;
-
-    public static final Parcelable.Creator CREATOR = new Parcelable.Creator()
-    {
-        public BookingPricesForFrequenciesResponse createFromParcel(final Parcel in)
-        {
-            return new BookingPricesForFrequenciesResponse(in);
-        }
-
-        public BookingPricesForFrequenciesResponse[] newArray(final int size)
-        {
-            return new BookingPricesForFrequenciesResponse[size];
-        }
-    };
 
     //this function is needed because server returns formatted strings (e.g. $20) and no explicit currency char!
     public char getCurrencyChar()
     {
         String targetString = null;
-        if (priceFormatted != null) //need these checks just in case server doesn't populate all the values
-        {
-            targetString = priceFormatted;
-        }
-        else if (monthlyPriceFormatted != null)
+        if (monthlyPriceFormatted != null) //need these checks just in case server doesn't populate all the values
         {
             targetString = monthlyPriceFormatted;
         }
@@ -81,50 +49,13 @@ public class BookingPricesForFrequenciesResponse implements Parcelable //parcela
         return Float.parseFloat(formattedPrice.substring(1));
     }
 
-    public Map<Integer, float[]> getPriceMap()
+    public Map<Integer, Float> getPriceMap()
     {
-        Map<Integer, float[]> priceMap = new HashMap<>();
-        priceMap.put(0, new float[]{getDollarAmountFromFormattedPrice(priceFormatted), getDollarAmountFromFormattedPrice(discountPriceFormatted)});
-        priceMap.put(1, new float[]{getDollarAmountFromFormattedPrice(weeklyPriceFormatted), getDollarAmountFromFormattedPrice(weeklyDiscountPriceFormatted)});
-        priceMap.put(2, new float[]{getDollarAmountFromFormattedPrice(bimonthlyPriceFormatted), getDollarAmountFromFormattedPrice(bimonthlyDiscountPriceFormatted)});
-        priceMap.put(4, new float[]{getDollarAmountFromFormattedPrice(monthlyPriceFormatted), getDollarAmountFromFormattedPrice(monthlyDiscountPriceFormatted)});
+        Map<Integer, Float> priceMap = new HashMap<>();
+        priceMap.put(1, getDollarAmountFromFormattedPrice(weeklyPriceFormatted));
+        priceMap.put(2, getDollarAmountFromFormattedPrice(bimonthlyPriceFormatted));
+        priceMap.put(4, getDollarAmountFromFormattedPrice(monthlyPriceFormatted));
         return priceMap;
-    }
-
-    private BookingPricesForFrequenciesResponse(final Parcel in)
-    {
-        final String[] stringData = new String[9];
-        in.readStringArray(stringData);
-        priceFormatted = stringData[0];
-        weeklyPriceFormatted = stringData[1];
-        bimonthlyPriceFormatted = stringData[2];
-        monthlyPriceFormatted = stringData[3];
-        discountPriceFormatted = stringData[4];
-        weeklyDiscountPriceFormatted = stringData[5];
-        bimonthlyDiscountPriceFormatted = stringData[6];
-        monthlyDiscountPriceFormatted = stringData[7];
-        currentFrequency = Integer.parseInt(stringData[8]);
-    }
-
-    @Override
-    public int describeContents()
-    {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags)
-    {
-        dest.writeStringArray(new String[]{
-                priceFormatted,
-                weeklyPriceFormatted,
-                bimonthlyPriceFormatted,
-                monthlyPriceFormatted,
-                discountPriceFormatted,
-                weeklyDiscountPriceFormatted,
-                bimonthlyDiscountPriceFormatted,
-                monthlyDiscountPriceFormatted,
-                Integer.toString(currentFrequency)});
     }
 
     public int getCurrentFrequency()
--- app/src/main/java/com/handybook/handybook/ui/fragment/BookingEditFrequencyFragment.java
+++ app/src/main/java/com/handybook/handybook/ui/fragment/BookingEditFrequencyFragment.java
@@ -2,12 +2,12 @@ package com.handybook.handybook.ui.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.handybook.handybook.R;
 import com.handybook.handybook.constant.ActivityResult;
@@ -15,11 +15,12 @@ import com.handybook.handybook.constant.BundleKeys;
 import com.handybook.handybook.core.Booking;
 import com.handybook.handybook.core.BookingOption;
 import com.handybook.handybook.core.BookingPricesForFrequenciesResponse;
-import com.handybook.handybook.core.BookingTransaction;
 import com.handybook.handybook.core.BookingUpdateFrequencyTransaction;
 import com.handybook.handybook.event.HandyEvent;
+import com.handybook.handybook.ui.view.TitleHeaderView;
 import com.handybook.handybook.ui.widget.BookingOptionsSelectView;
 import com.handybook.handybook.ui.widget.BookingOptionsView;
+import com.handybook.handybook.util.TextUtils;
 import com.squareup.otto.Subscribe;
 
 import java.util.Map;
@@ -38,8 +39,8 @@ public final class BookingEditFrequencyFragment extends BookingFlowFragment
     LinearLayout optionsLayout;
     @Bind(R.id.next_button)
     Button nextButton;
-
-//    private BookingOptionsSelectView mOptionsView;
+    @Bind(R.id.nav_text)
+    TextView navText;
 
     public static BookingEditFrequencyFragment newInstance(Booking booking)
     {
@@ -82,6 +83,12 @@ public final class BookingEditFrequencyFragment extends BookingFlowFragment
                 .inflate(R.layout.fragment_booking_recurrence, container, false);
 
         ButterKnife.bind(this, view);
+        ViewGroup infoHeaderHolder = (ViewGroup) getActivity().findViewById(R.id.info_header_layout);
+        infoHeaderHolder.removeAllViews();
+        TitleHeaderView newView = (TitleHeaderView) getActivity().getLayoutInflater().inflate(R.layout.layout_title_header, null);
+        infoHeaderHolder.addView(newView);
+        newView.setTitleString(getResources().getString(R.string.how_often_should_come));
+        navText.setText(R.string.edit_frequency);
         nextButton.setText(R.string.update);
         nextButton.setOnClickListener(nextClicked);
         return view;
@@ -138,66 +145,6 @@ public final class BookingEditFrequencyFragment extends BookingFlowFragment
         }
     };
 
-    //nearly completely copy-pasted from BookingRecurrenceFragment
-    //TODO: the discounts displayed here may seem misleading because the price for the one-time booking will not be displayed for user to compare. what to do?
-    private String[] getSavingsInfo(Map<Integer, float[]> priceMap)
-    {
-        final String[] info = new String[4];
-        final float prices[] = priceMap.get(0);//get price for one-time booking
-        final float price = prices[0];
-        final float discount = prices[1];
-
-        for (int i = 0; i < 3; i++) {
-            final float recurPrices[] = priceMap.get(freqForIndex(i));
-            final float recurPrice = recurPrices[0];
-            final float recurDiscount = recurPrices[1];
-
-            int percent;
-            if (recurPrice != recurDiscount)
-                percent = (int)((discount - recurDiscount) / discount * 100);
-            else percent = (int)((price - recurPrice) / price * 100);
-
-            if (percent > 0) info[i] = getString(R.string.save) + " " + percent + "%";
-        }
-        return info;
-    }
-
-    private int indexForFreq(final int freq)
-    {
-        switch (freq)
-        {
-            case 1:
-                return 0;
-
-            case 2:
-                return 1;
-
-            case 4:
-                return 2;
-
-            default:
-                return 3;
-        }
-    }
-
-    private int freqForIndex(final int index)
-    {
-        switch (index)
-        {
-            case 0:
-                return 1;
-
-            case 1:
-                return 2;
-
-            case 2:
-                return 4;
-
-            default:
-                return 0;
-        }
-    }
-
     private void showUiBlockers()
     {
         disableInputs();
@@ -210,43 +157,36 @@ public final class BookingEditFrequencyFragment extends BookingFlowFragment
         progressDialog.dismiss();
     }
 
+    private String[] getOriginalPriceArrayForRecurValues(BookingPricesForFrequenciesResponse bookingPricesForFrequenciesResponse)
+    {
+        String[] priceArray = new String[recurValues.length];
+        Map<Integer, Float> priceMap = bookingPricesForFrequenciesResponse.getPriceMap();
+        char currencyChar = bookingPricesForFrequenciesResponse.getCurrencyChar();
+        for(int i = 0; i<priceArray.length; i++)
+        {
+            float originalPrice = priceMap.get(recurValues[i]);
+            priceArray[i] = TextUtils.formatPrice(originalPrice, Character.toString(currencyChar), "");
+        }
+        return priceArray;
+    }
     private void updateUiWithBookingPricesForFrequencies(BookingPricesForFrequenciesResponse bookingPricesForFrequenciesResponse)
     {
-        //update the discount % display to the right of the options text
+        //update the options right text
         final BookingOption option = new BookingOption();
         option.setType(BookingOption.TYPE_OPTION);
         option.setDefaultValue("0");
-
         option.setOptions(new String[]{getString(R.string.every_week),
                 getString(R.string.every_two_weeks), getString(R.string.every_four_weeks)});
-
         recurValues = new int[]{1, 2, 4}; //allowing edit frequency only for recurring bookings
-
         option.setOptionsSubText(new String[]
                 {null, getString(R.string.most_popular), null});
-
-        option.setOptionsRightText(getSavingsInfo(bookingPricesForFrequenciesResponse.getPriceMap())); //TODO: need BookingQuote's price table to get savings info
-
+        option.setOptionsRightText(getOriginalPriceArrayForRecurValues(bookingPricesForFrequenciesResponse));
         final BookingOptionsSelectView optionsView
                 = new BookingOptionsSelectView(getActivity(), option, optionUpdated);
         optionsView.hideTitle();
 
         optionsLayout.removeAllViews();
         optionsLayout.addView(optionsView, 0);
-
-
-        //this is gross, need to call bookingManager.setCurrentTransaction because BookingHeaderFragment uses bookingManager.getCurrentTransaction to do a lot of things
-        BookingTransaction bookingTransaction = new BookingTransaction();
-        bookingTransaction.setHours(booking.getHours());
-        bookingTransaction.setRecurringFrequency(bookingPricesForFrequenciesResponse.getCurrentFrequency());
-        optionsView.setCurrentIndex(indexForFreq(bookingPricesForFrequenciesResponse.getCurrentFrequency()));
-        bookingTransaction.setStartDate(booking.getStartDate());
-        bookingManager.setCurrentTransaction(bookingTransaction);
-
-        //set the header fragment. need to do it here because we need the price for each booking frequency
-        final BookingHeaderFragment header = BookingHeaderFragment.newInstance(bookingPricesForFrequenciesResponse);
-        final FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
-        transaction.replace(R.id.info_header_layout, header).commit();
     }
 
     private void onReceiveErrorEvent(HandyEvent.ReceiveErrorEvent event)
