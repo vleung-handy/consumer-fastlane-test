37a1ed65f61056db5f962b515a40d4802f62cefd
Victoria Leung <vleung@handybook.com>
2015-11-12 13:44:05 -0500

    espresso gradle setup + super simple test class

--- app/src/androidTest/java/com/handybook/handybook/LoginTest.java
+++ app/src/androidTest/java/com/handybook/handybook/LoginTest.java
@@ -1,42 +1,29 @@
 package com.handybook.handybook;
 
-import android.support.test.InstrumentationRegistry;
-import android.test.ActivityInstrumentationTestCase2;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import com.handybook.handybook.ui.activity.LoginActivity;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-//proof of concept espresso test
-public class LoginTest extends ActivityInstrumentationTestCase2<LoginActivity>{
+//super simple espresso test for proof of concept only
+@RunWith(AndroidJUnit4.class)
+public class LoginTest{
 
-    private LoginActivity mActivity;
-
-    public LoginTest() {
-        super(LoginActivity.class);
-    }
-
-    @Before
-    public void setUp() throws Exception {
-        super.setUp();
-        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-        mActivity = getActivity();
-    }
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule(LoginActivity.class);
 
     @Test
-    public void LoginVisibleTest() {
+    public void LoginHeaderVisibleTest() {
         onView(withId(R.id.nav_text)).check(matches(withText("Log In")));
     }
 
-    @After
-    public void tearDown() throws Exception {
-        super.tearDown();
-    }
 }
