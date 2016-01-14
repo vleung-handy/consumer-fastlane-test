769d5b8137031a8a445889adcf84a5434424c1a2
Roumwelle Sta Ines <rstaines@handy.com>
2016-01-14 18:30:03 -0500

    Referral UI

--- app/src/main/java/com/handybook/handybook/module/referral/ui/ReferralFragment.java
+++ app/src/main/java/com/handybook/handybook/module/referral/ui/ReferralFragment.java
@@ -1,13 +1,66 @@
 package com.handybook.handybook.module.referral.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.handybook.handybook.R;
 import com.handybook.handybook.ui.fragment.InjectedFragment;
+import com.handybook.handybook.ui.widget.MenuButton;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ReferralFragment extends InjectedFragment
 {
+    @Bind(R.id.menu_button_layout)
+    ViewGroup mMenuButtonLayout;
+    @Bind(R.id.title)
+    TextView mTitle;
+    @Bind(R.id.subtitle)
+    TextView mSubtitle;
+    @Bind(R.id.code)
+    TextView mCode;
+
     public static Fragment newInstance()
     {
         return new ReferralFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            final LayoutInflater inflater, final ViewGroup container,
+            final Bundle savedInstanceState
+    )
+    {
+        final View view = inflater.inflate(R.layout.fragment_referral, container, false);
+        ButterKnife.bind(this, view);
+
+        final MenuButton menuButton = new MenuButton(getActivity(), mMenuButtonLayout);
+        menuButton.setColor(getResources().getColor(R.color.white));
+        mMenuButtonLayout.addView(menuButton);
+
+        mTitle.setText("Give $35, Get $35");
+        mSubtitle.setText("Give your friends $35 off their first Handy booking, and you get $35!");
+        mCode.setText("JOHN1234");
+
+        return view;
+    }
+
+    @OnClick(R.id.share_button)
+    public void onShareButtonClicked()
+    {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "JOHN1234");
+        sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.share_to)));
     }
 }
