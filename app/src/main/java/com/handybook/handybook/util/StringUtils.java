c9093898270303fe4555767abfca51b7b4c3c278
Ondrej Dolejsi <odolejsi@handy.com>
2016-02-02 18:36:26 -0500

    New readAssetFile method is a quick and dirty way to read mock json files for example from asset folder.

--- app/src/main/java/com/handybook/handybook/util/StringUtils.java
+++ app/src/main/java/com/handybook/handybook/util/StringUtils.java
@@ -1,5 +1,13 @@
 package com.handybook.handybook.util;
 
+import android.content.Context;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+
 public class StringUtils
 {
     public static String capitalizeFirstCharacter(String s)
@@ -11,5 +19,50 @@ public class StringUtils
             returnValue += s.substring(1);
         }
         return returnValue;
+    }
+
+    public static String readAssetFile(final Context ctx, final String filename)
+    {
+        InputStream input = null;
+        try
+        {
+            input = ctx.getAssets().open(filename);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        Reader reader = null;
+        try
+        {
+            reader = new InputStreamReader(input, "UTF-8");
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+        StringBuilder sb = new StringBuilder();
+        char buffer[] = new char[16384];  // read 16k blocks
+        int len; // how much content was read?
+        try
+        {
+            while ((len = reader.read(buffer)) > 0)
+            {
+                sb.append(buffer, 0, len);
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        try
+        {
+            reader.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return sb.toString();
     }
 }
