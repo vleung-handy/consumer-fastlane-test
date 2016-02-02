f2d1b1d01c8acb1ae3eb462198d9ed43d9ee0782
Ondrej Dolejsi <odolejsi@handy.com>
2016-02-02 18:37:58 -0500

    Loading mock data from assets and prepared some methods for DragAndDropVerticalLinearLayout.java

--- app/src/main/java/com/handybook/handybook/booking/bookingedit/ui/activity/DevelopDragAndDropVerticalLinearLayoutActivity.java
+++ app/src/main/java/com/handybook/handybook/booking/bookingedit/ui/activity/DevelopDragAndDropVerticalLinearLayoutActivity.java
@@ -2,16 +2,43 @@ package com.handybook.handybook.booking.bookingedit.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ScrollView;
 
+import com.google.gson.Gson;
 import com.handybook.handybook.R;
+import com.handybook.handybook.booking.model.ChecklistItem;
+import com.handybook.handybook.booking.model.Instructions;
+import com.handybook.handybook.booking.ui.widget.BookingInstructionView;
+import com.handybook.handybook.booking.ui.widget.DragAndDropVerticalLinearLayout;
+import com.handybook.handybook.util.StringUtils;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class DevelopDragAndDropVerticalLinearLayoutActivity extends AppCompatActivity
 {
+    @Bind(R.id.develop_scrollview)
+    ScrollView mScrollView;
+    @Bind(R.id.develop_dnd)
+    DragAndDropVerticalLinearLayout mDndLayout;
+
+    private Instructions mInstructions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout._develop_drag_and_drop_vertical_linear_layout);
+        ButterKnife.bind(this);
+        String jsonInstructions = StringUtils.readAssetFile(this, "mock_instructions.json");
+        mInstructions = new Gson().fromJson(jsonInstructions, Instructions.class);
+        mDndLayout.removeAllViews();
+        mDndLayout.linkScrollView(mScrollView);
+        for (ChecklistItem eCheckListItem : mInstructions.getChecklist())
+        {
+            BookingInstructionView biv = new BookingInstructionView(this);
+            biv.reflect(eCheckListItem);
+            mDndLayout.addView(biv);
+        }
     }
 }
--- app/src/main/java/com/handybook/handybook/booking/ui/widget/DragAndDropVerticalLinearLayout.java
+++ app/src/main/java/com/handybook/handybook/booking/ui/widget/DragAndDropVerticalLinearLayout.java
@@ -4,10 +4,18 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class DragAndDropVerticalLinearLayout extends LinearLayout
 {
+    public static String CLASS_TAG = DragAndDropVerticalLinearLayout.class.getSimpleName();
+    /**
+     * Linked scrollview to be scrolled while dragging items
+     */
+    private ScrollView mScrollView;
+
     public DragAndDropVerticalLinearLayout(final Context context)
     {
         super(context);
@@ -37,4 +45,17 @@ public class DragAndDropVerticalLinearLayout extends LinearLayout
     {
     }
 
+    public void linkScrollView(final ScrollView scrollView)
+    {
+        mScrollView = scrollView;
+    }
+
+    public void moveChild( final int fromPosition, final int toPosition){
+        Log.v(CLASS_TAG, "moveChild(" + fromPosition+", " + toPosition +")");
+        //TODO: Implement
+    }
+    public void swapChildren( final int positionA, final int positionB){
+        Log.v(CLASS_TAG, "swapChildren(" + positionA + ", " + positionB + ")");
+        //TODO: Implement
+    }
 }
