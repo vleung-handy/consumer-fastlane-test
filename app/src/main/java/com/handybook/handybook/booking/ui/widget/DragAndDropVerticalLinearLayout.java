2407a265544dce5de39fda814f3c0c20702536a3
Ondrej Dolejsi <odolejsi@handy.com>
2016-02-03 13:18:33 -0500

    New scroll zones to let the user scroll while dragging an instruction.

--- app/src/main/java/com/handybook/handybook/booking/ui/widget/DragAndDropVerticalLinearLayout.java
+++ app/src/main/java/com/handybook/handybook/booking/ui/widget/DragAndDropVerticalLinearLayout.java
@@ -1,9 +1,11 @@
 package com.handybook.handybook.booking.ui.widget;
 
+import android.animation.ObjectAnimator;
 import android.annotation.TargetApi;
 import android.content.ClipData;
 import android.content.Context;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.SparseArray;
@@ -16,19 +18,25 @@ import android.widget.ScrollView;
 
 public class DragAndDropVerticalLinearLayout extends LinearLayout
 {
+    public static String CLASS_TAG = DragAndDropVerticalLinearLayout.class.getSimpleName();
+
     public static final int ABOVE = -1;
     public static final int BELOW = 1;
-    public static String CLASS_TAG = DragAndDropVerticalLinearLayout.class.getSimpleName();
+
     /**
      * Linked scrollview to be scrolled while dragging items
      */
     private ScrollView mScrollView;
+    private ScrollZone mTopScrollZone;
+    private ScrollZone mBottomScrollZone;
     private SparseArray<View> mDraggableChildren = new SparseArray<>();
     private OnLongClickListener mLongClickListener = new OnChildLongClickListener();
     private boolean mIsDragging;
     private View mViewBeingDragged;
     private boolean mIsDraggingEnabled;
     private DragShadowBuilder mShadowBuilder;
+    private boolean mIsScrolling;
+    private ObjectAnimator mScrollAnimator;
 
 
     public DragAndDropVerticalLinearLayout(final Context context)
@@ -71,7 +79,7 @@ public class DragAndDropVerticalLinearLayout extends LinearLayout
             @Override
             public boolean onDrag(final View view, final DragEvent event)
             {
-                Log.v(CLASS_TAG, "DragEvent: " + event.toString());
+                //Log.v(CLASS_TAG, "DragEvent: " + event.toString());
                 int action = event.getAction();
                 switch (action)
                 {
@@ -129,17 +137,81 @@ public class DragAndDropVerticalLinearLayout extends LinearLayout
     private void updateGhostLocation(final float x, final float y)
     {
         Log.v(CLASS_TAG, "updateGhostLocation(" + x + "," + y + ")");
-        if (isAboveUpperBoundary(y))
+        updateScrollView(x, y);
+        if (shouldSwapWithViewAbove(y))
         {
             swapChildren(getNeighbor(mViewBeingDragged, ABOVE), mViewBeingDragged);
         }
-        else if (isBelowLowerBoundary(y))
+        else if (shouldSwapWithViewBelow(y))
         {
             swapChildren(mViewBeingDragged, getNeighbor(mViewBeingDragged, BELOW));
         }
     }
 
-    private boolean isAboveUpperBoundary(final float y)
+    private void updateScrollView(final float x, final float y)
+    {
+        if (mScrollView == null)
+        {
+            return;
+        }
+        updateScrollZones(x, y);
+        if (mTopScrollZone.isTriggeredBy(x, y) || mBottomScrollZone.isTriggeredBy(x, y))
+        {
+            if (mIsScrolling)
+            {
+                //keep scrolling
+            }
+            else
+            {
+                startScrolling(x, y);
+            }
+        }
+        else
+        {
+            stopScrolling();
+        }
+
+    }
+
+    private void startScrolling(final float x, final float y)
+    {
+        if (mScrollAnimator != null && mScrollAnimator.isRunning())
+        {
+            mScrollAnimator.cancel();
+        }
+        if (mTopScrollZone.isTriggeredBy(x, y))
+        {
+            int min = 0;
+            mScrollAnimator = ObjectAnimator.ofInt(mScrollView, "scrollY", min).setDuration(1000);
+            mScrollAnimator.start();
+            mIsScrolling = true;
+        }
+        else if (mBottomScrollZone.isTriggeredBy(x, y))
+        {
+            int max = mScrollView.getMaxScrollAmount();
+            mScrollAnimator = ObjectAnimator.ofInt(mScrollView, "scrollY", max).setDuration(1000);
+            mScrollAnimator.start();
+            mIsScrolling = true;
+        }
+    }
+
+    private void stopScrolling()
+    {
+        if (mScrollAnimator == null)
+        {
+            return;
+        }
+        mScrollAnimator.cancel();
+        mIsScrolling = false;
+    }
+
+    private void updateScrollZones(final float x, final float y)
+    {
+        mTopScrollZone.update();
+        mBottomScrollZone.update();
+    }
+
+    private boolean shouldSwapWithViewAbove(final float y)
     {
         View neighborAbove = getNeighbor(mViewBeingDragged, ABOVE);
         if (neighborAbove == null)
@@ -149,7 +221,7 @@ public class DragAndDropVerticalLinearLayout extends LinearLayout
         return neighborAbove != null && y < neighborAbove.getY() + neighborAbove.getHeight() / 2;
     }
 
-    private boolean isBelowLowerBoundary(final float y)
+    private boolean shouldSwapWithViewBelow(final float y)
     {
         View neighborBelow = getNeighbor(mViewBeingDragged, BELOW);
         return neighborBelow != null && y > neighborBelow.getY() + neighborBelow.getHeight() / 2;
@@ -200,6 +272,8 @@ public class DragAndDropVerticalLinearLayout extends LinearLayout
     public void linkScrollView(final ScrollView scrollView)
     {
         mScrollView = scrollView;
+        mTopScrollZone = new ScrollZone(this, mScrollView, ScrollZone.ScrollZoneType.TOP);
+        mBottomScrollZone = new ScrollZone(this, mScrollView, ScrollZone.ScrollZoneType.BOTTOM);
     }
 
     public void moveChild(final int fromIndex, final int toIndex)
@@ -276,7 +350,83 @@ public class DragAndDropVerticalLinearLayout extends LinearLayout
         }
     }
 
-    private class OnChildLongClickListener implements OnLongClickListener
+
+    private static class ScrollZone
+    {
+        private static int SCROLL_ZONE_HEIGHT = 64;
+
+        private final View mView;
+        private final ScrollView mScrollView;
+        private final ScrollZoneType mScrollZoneType;
+        private float mTop, mRight, mBottom, mLeft;
+
+
+        public enum ScrollZoneType
+        {
+            TOP, BOTTOM
+        }
+
+
+        public ScrollZone(
+                @NonNull final View root,
+                @NonNull final ScrollView scrollView,
+                @NonNull ScrollZoneType type
+        )
+        {
+            mView = root;
+            mScrollView = scrollView;
+            mScrollZoneType = type;
+            update();
+        }
+
+        public void update()
+        {
+            switch (mScrollZoneType)
+            {
+                case TOP:
+                    updateTop();
+                    break;
+                case BOTTOM:
+                    updateBottom();
+                    break;
+            }
+        }
+
+        private void updateTop()
+        {
+            mTop = Math.max(mScrollView.getScrollY() - mView.getTop(), 0);
+            mBottom = mTop + SCROLL_ZONE_HEIGHT;
+            mLeft = 0;
+            mRight = mView.getWidth();
+            Log.v("TopScrollZone", this.toString());
+        }
+
+        private void updateBottom()
+        {
+            mBottom = Math.min(
+                    mScrollView.getHeight() - mView.getY() + mScrollView.getScrollY(),
+                    mView.getHeight());
+            mTop = mBottom - SCROLL_ZONE_HEIGHT;
+            mLeft = 0;
+            mRight = mView.getWidth();
+            Log.v("BottomScrollZone", this.toString());
+        }
+
+        public boolean isTriggeredBy(final float x, final float y)
+        {
+            return x >= mLeft && x <= mRight && y >= mTop && y <= mBottom;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "ScrollZone:[top:" + mTop + ", right: " + mRight
+                    + ", bottom:" + mBottom + ", left:" + mLeft + "]";
+        }
+    }
+
+
+    private class OnChildLongClickListener implements View.OnLongClickListener
     {
         @Override
         public boolean onLongClick(final View view)
