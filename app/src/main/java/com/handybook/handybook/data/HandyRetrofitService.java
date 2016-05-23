1f17973f99b3234875dc688c3b0110e6684c956b
Ondrej Dolejsi <odolejsi@handy.com>
2016-05-23 16:48:02 -0400

    HandyRetrofitService: added parameter to ratePro method (match_preference)

--- app/src/main/java/com/handybook/handybook/data/HandyRetrofitService.java
+++ app/src/main/java/com/handybook/handybook/data/HandyRetrofitService.java
@@ -32,24 +32,30 @@ import retrofit.mime.TypedInput;
 public interface HandyRetrofitService
 {
     @GET("/promos")
-    void getAvailableSplashPromo(@Query("user_id") String userId,
-                                 @Query("displayed_promos[]") String[] displayedPromos,
-                                 @Query("accepted_promos[]") String[] acceptedPromos,
-                                 HandyRetrofitCallback cb);
+    void getAvailableSplashPromo(
+            @Query("user_id") String userId,
+            @Query("displayed_promos[]") String[] displayedPromos,
+            @Query("accepted_promos[]") String[] acceptedPromos,
+            HandyRetrofitCallback cb
+    );
 
     @POST("/bookings/{id}/address_update")
-    void editBookingAddress(@Path("id") int bookingId,
-                            @Body BookingEditAddressRequest bookingEditAddressRequest,
-                            HandyRetrofitCallback cb);
+    void editBookingAddress(
+            @Path("id") int bookingId,
+            @Body BookingEditAddressRequest bookingEditAddressRequest,
+            HandyRetrofitCallback cb
+    );
+
     /**
-     *
      * @param bookingRecurringId Booking.recurringId, which is the id
      *                           associated with a recurring series
-     * @param cb
+     * @param cb callback
      */
     @POST("/bookings/{id}/recurring_cancel_send_cancel_email")
-    void sendCancelRecurringBookingEmail(@Path("id") int bookingRecurringId,
-                                         HandyRetrofitCallback cb);
+    void sendCancelRecurringBookingEmail(
+            @Path("id") int bookingRecurringId,
+            HandyRetrofitCallback cb
+    );
 
     @GET("/app_updates")
     void getBlockedWrapper(@Query("version_code") int versionCode, HandyRetrofitCallback cb);
@@ -64,27 +70,35 @@ public interface HandyRetrofitService
     void getEditExtrasInfo(@Path("id") int bookingId, HandyRetrofitCallback cb);
 
     @POST("/bookings/{id}/edit_extras")
-    void editServiceExtras(@Path("id") int bookingId,
-                          @Body BookingEditExtrasRequest bookingEditExtrasRequest,
-                          HandyRetrofitCallback cb);
+    void editServiceExtras(
+            @Path("id") int bookingId,
+            @Body BookingEditExtrasRequest bookingEditExtrasRequest,
+            HandyRetrofitCallback cb
+    );
 
     @GET("/quotes/new")
-    void getQuoteOptions(@Query("service_id") int serviceId, @Query("user_id") String userId,
-                         HandyRetrofitCallback cb);
+    void getQuoteOptions(
+            @Query("service_id") int serviceId, @Query("user_id") String userId,
+            HandyRetrofitCallback cb
+    );
 
     @POST("/quotes")
     void createQuote(@Body BookingRequest req, HandyRetrofitCallback cb);
 
     @FormUrlEncoded
     @POST("/quotes/{quote}/select_new_time")
-    void updateQuoteDate(@Path("quote") int quoteId, @Field("date_start") Date date,
-                         HandyRetrofitCallback cb);
+    void updateQuoteDate(
+            @Path("quote") int quoteId, @Field("date_start") Date date,
+            HandyRetrofitCallback cb
+    );
 
     @FormUrlEncoded
     @POST("/quotes/{quote}/set_coupon")
-    void applyPromo(@Field("coupon") String promoCode, @Path("quote") int quoteId,
-                    @Field("user_id") String userId, @Field("email") String email,
-                    @Field("auth_token") String authToken, HandyRetrofitCallback cb);
+    void applyPromo(
+            @Field("coupon") String promoCode, @Path("quote") int quoteId,
+            @Field("user_id") String userId, @Field("email") String email,
+            @Field("auth_token") String authToken, HandyRetrofitCallback cb
+    );
 
     @POST("/quotes/{quote}/remove_coupon")
     void removePromo(@Path("quote") int quoteId, HandyRetrofitCallback cb);
@@ -93,9 +107,11 @@ public interface HandyRetrofitService
     void createBooking(@Path("quote") int quoteId, @Body BookingTransaction req, HandyRetrofitCallback cb);
 
     @GET("/bookings/zipcode_validation")
-    void validateBookingZip(@Query("service_id") int serviceId, @Query("zipcode") String zipCode,
-                            @Query("user_id") String userId, @Query("auth_token") String authToken,
-                            @Query("entered_code") String promoCode, HandyRetrofitCallback cb);
+    void validateBookingZip(
+            @Query("service_id") int serviceId, @Query("zipcode") String zipCode,
+            @Query("user_id") String userId, @Query("auth_token") String authToken,
+            @Query("entered_code") String promoCode, HandyRetrofitCallback cb
+    );
 
     @GET("/bookings")
     void getBookings(
@@ -105,17 +121,23 @@ public interface HandyRetrofitService
     );
 
     @GET("/bookings/{bookingId}/edit_hours")
-    void getEditHoursInfo(@Path("bookingId") int bookingId,
-                    HandyRetrofitCallback cb);
+    void getEditHoursInfo(
+            @Path("bookingId") int bookingId,
+            HandyRetrofitCallback cb
+    );
 
     @POST("/bookings/{bookingId}/edit_hours")
-    void editBookingHours(@Path("bookingId") int bookingId,
-                           @Body BookingEditHoursRequest bookingEditHoursRequest,
-                           HandyRetrofitCallback cb);
+    void editBookingHours(
+            @Path("bookingId") int bookingId,
+            @Body BookingEditHoursRequest bookingEditHoursRequest,
+            HandyRetrofitCallback cb
+    );
 
     @GET("/bookings/{id}")
-    void getBooking(@Path("id") String bookingId,
-                    HandyRetrofitCallback cb);
+    void getBooking(
+            @Path("id") String bookingId,
+            HandyRetrofitCallback cb
+    );
 
     @GET("/bookings/promo_prebooking")
     void getPreBookingPromo(@Query("code") String promoCode, HandyRetrofitCallback cb);
@@ -130,40 +152,56 @@ public interface HandyRetrofitService
 
     @FormUrlEncoded
     @POST("/bookings/{booking}/rate_pro")
-    void ratePro(@Path("booking") int bookingId, @Field("rating_int") int rating,
-                 @Field("tip_amount") Integer tipAmount,
-                 HandyRetrofitCallback cb);
+    void ratePro(
+            @Path("booking") int bookingId,
+            @Field("rating_int") int rating,
+            @Field("tip_amount") Integer tipAmount,
+            @Field("match_preference") String proMatchPreference,
+            HandyRetrofitCallback cb
+    );
 
     @FormUrlEncoded
     @POST("/bookings/{booking}/tip")
-    void tipPro(@Path("booking") int bookingId, @Field("tip_amount") Integer tipAmount,
-                HandyRetrofitCallback cb);
+    void tipPro(
+            @Path("booking") int bookingId, @Field("tip_amount") Integer tipAmount,
+            HandyRetrofitCallback cb
+    );
 
     @POST("/bookings/{booking}/rating_flow")
-    void submitProRatingDetails(@Path("booking") int bookingId, @Body RateProRequest req,
-                                HandyRetrofitCallback cb);
+    void submitProRatingDetails(
+            @Path("booking") int bookingId, @Body RateProRequest req,
+            HandyRetrofitCallback cb
+    );
 
     @POST("/bookings/{booking}/after_booking_update")
-    void addBookingPostInfo(@Path("booking") int bookingId, @Body BookingPostInfo info,
-                            HandyRetrofitCallback cb);
+    void addBookingPostInfo(
+            @Path("booking") int bookingId, @Body BookingPostInfo info,
+            HandyRetrofitCallback cb
+    );
 
 
     @POST("/bookings/{booking}/description_update")
         //points to same endpoint as update entry info but that is because the endpoint currently does too much
-    void updateBookingNoteToPro(@Path("booking") int bookingId,
-                                @Body BookingUpdateNoteToProTransaction descriptionTransaction,
-                                HandyRetrofitCallback cb);
+    void updateBookingNoteToPro(
+            @Path("booking") int bookingId,
+            @Body BookingUpdateNoteToProTransaction descriptionTransaction,
+            HandyRetrofitCallback cb
+    );
 
     @POST("/bookings/{booking}/description_update")
         //points to same endpoint as update note to pro but that is because the endpoint currently does too much
-    void updateBookingEntryInformation(@Path("booking") int bookingId,
-                                       @Body BookingUpdateEntryInformationTransaction entryInformationTransaction,
-                                       HandyRetrofitCallback cb);
+    void updateBookingEntryInformation(
+            @Path("booking") int bookingId,
+            @Body BookingUpdateEntryInformationTransaction entryInformationTransaction,
+            HandyRetrofitCallback cb
+    );
 
     @POST("/bookings/{booking}/finalize_booking")
-    void finalizeBooking(@Path("booking") int bookingId,
-                                       @Body FinalizeBookingRequestPayload finalizeBookingRequestPayload,
-                                       HandyRetrofitCallback cb);
+    void finalizeBooking(
+            @Path("booking") int bookingId,
+            @Body FinalizeBookingRequestPayload finalizeBookingRequestPayload,
+            HandyRetrofitCallback cb
+    );
 
     @POST("/bookings/{booking}/preferences")
     void updatePreferences(
@@ -173,64 +211,84 @@ public interface HandyRetrofitService
     );
 
     @POST("/bookings/{booking}/edit_frequency")
-    void updateBookingFrequency(@Path("booking") int bookingId,
-                                @Body BookingEditFrequencyRequest bookingEditFrequencyRequest,
-                                HandyRetrofitCallback cb);
+    void updateBookingFrequency(
+            @Path("booking") int bookingId,
+            @Body BookingEditFrequencyRequest bookingEditFrequencyRequest,
+            HandyRetrofitCallback cb
+    );
 
     @GET("/bookings/{booking}/edit_frequency")
-    void getBookingPricesForFrequencies(@Path("booking") int bookingId,
-                                        HandyRetrofitCallback cb);
+    void getBookingPricesForFrequencies(
+            @Path("booking") int bookingId,
+            HandyRetrofitCallback cb
+    );
 
     @GET("/bookings/prereschedule_info")
     void getPreRescheduleInfo(@Query("booking_id") String bookingId, HandyRetrofitCallback cb);
 
     @FormUrlEncoded
     @POST("/bookings/{booking}/reschedule")
-    void rescheduleBooking(@Path("booking") String bookingId, @Field("new_date") String date,
-                           @Field("reschedule_all") int rescheduleAll, @Field("user_id") String userId,
-                           @Field("auth_token") String authToken, HandyRetrofitCallback cb);
+    void rescheduleBooking(
+            @Path("booking") String bookingId, @Field("new_date") String date,
+            @Field("reschedule_all") int rescheduleAll, @Field("user_id") String userId,
+            @Field("auth_token") String authToken, HandyRetrofitCallback cb
+    );
 
     @GET("/bookings/precancelation_info")
     void getPreCancelationInfo(@Query("booking_id") String bookingId, HandyRetrofitCallback cb);
 
     @FormUrlEncoded
     @POST("/bookings/{booking}/cancel")
-    void cancelBooking(@Path("booking") String bookingId, @Field("cancellation_reason") int reasonCode,
-                       @Field("user_id") String userId, @Field("auth_token") String authToken,
-                       HandyRetrofitCallback cb);
+    void cancelBooking(
+            @Path("booking") String bookingId, @Field("cancellation_reason") int reasonCode,
+            @Field("user_id") String userId, @Field("auth_token") String authToken,
+            HandyRetrofitCallback cb
+    );
 
     @FormUrlEncoded
     @POST("/bookings/{booking}/cancel")
-    void cancelBooking(@Path("booking") String bookingId, @Field("user_id") String userId,
-                       @Field("auth_token") String authToken, HandyRetrofitCallback cb);
+    void cancelBooking(
+            @Path("booking") String bookingId, @Field("user_id") String userId,
+            @Field("auth_token") String authToken, HandyRetrofitCallback cb
+    );
 
     @GET("/bookings/{booking}/schedule_laundry")
-    void getLaundryScheduleInfo(@Path("booking") int bookingId, @Query("auth_token") String authToken,
-                                HandyRetrofitCallback cb);
+    void getLaundryScheduleInfo(
+            @Path("booking") int bookingId, @Query("auth_token") String authToken,
+            HandyRetrofitCallback cb
+    );
 
     @FormUrlEncoded
     @POST("/bookings/{booking}/schedule_laundry")
-    void setLaundryDropOff(@Path("booking") int bookingId, @Field("auth_token") String authToken,
-                           @Field("date") String date, @Field("hour") int hour,
-                           @Field("minute") int minute, @Field("type") String type,
-                           HandyRetrofitCallback cb);
+    void setLaundryDropOff(
+            @Path("booking") int bookingId, @Field("auth_token") String authToken,
+            @Field("date") String date, @Field("hour") int hour,
+            @Field("minute") int minute, @Field("type") String type,
+            HandyRetrofitCallback cb
+    );
 
     @GET("/bookings/{booking}/add_laundry")
-    void getAddLaundryInfo(@Path("booking") int bookingId, @Query("auth_token") String authToken,
-                           HandyRetrofitCallback cb);
+    void getAddLaundryInfo(
+            @Path("booking") int bookingId, @Query("auth_token") String authToken,
+            HandyRetrofitCallback cb
+    );
 
     @FormUrlEncoded
     @POST("/bookings/{booking}/add_laundry")
-    void addLaundry(@Path("booking") int bookingId, @Field("auth_token") String authToken,
-                    HandyRetrofitCallback cb);
+    void addLaundry(
+            @Path("booking") int bookingId, @Field("auth_token") String authToken,
+            HandyRetrofitCallback cb
+    );
 
     @GET("/recurring_bookings")
     void getRecurringBookings(HandyRetrofitCallback cb);
 
     @FormUrlEncoded
     @POST("/user_sessions")
-    void createUserSession(@Field("email") String email, @Field("password") String password,
-                           HandyRetrofitCallback cb);
+    void createUserSession(
+            @Field("email") String email, @Field("password") String password,
+            HandyRetrofitCallback cb
+    );
 
     @POST("/user_sessions/fb_create")
     void createUserSessionFB(@Body CreateUserRequest createUserRequest, HandyRetrofitCallback cb);
@@ -239,19 +297,25 @@ public interface HandyRetrofitService
     void createUser(@Body CreateUserRequest createUserRequest, HandyRetrofitCallback cb);
 
     @GET("/users/{user}")
-    void getUserInfo(@Path("user") String userId, @Query("auth_token") String authToken,
-                     HandyRetrofitCallback cb);
+    void getUserInfo(
+            @Path("user") String userId, @Query("auth_token") String authToken,
+            HandyRetrofitCallback cb
+    );
 
     @GET("/users/exists")
     void getUserExists(@Query("email") String email, HandyRetrofitCallback cb);
 
     @PUT("/users/{user}")
-    void updateUserInfo(@Path("user") String userId, @Body UpdateUserRequest req,
-                        HandyRetrofitCallback cb);
+    void updateUserInfo(
+            @Path("user") String userId, @Body UpdateUserRequest req,
+            HandyRetrofitCallback cb
+    );
 
     @PUT("/users/{user_id}/update_credit_card")
-    void updatePaymentInfo(@Path("user_id") String userId, @Query("stripe_token") String token,
-                           HandyRetrofitCallback cb);
+    void updatePaymentInfo(
+            @Path("user_id") String userId, @Query("stripe_token") String token,
+            HandyRetrofitCallback cb
+    );
 
     @GET("/configuration")
     void requestConfiguration(HandyRetrofitCallback cb);
@@ -264,36 +328,44 @@ public interface HandyRetrofitService
             @Query("since_id") Long sinceId,
             @Query("until_id") Long untilId,
             HandyRetrofitCallback cb
-            );
+    );
 
     @GET("/password_resets/new")
     void requestPasswordReset(@Query("email") String email, HandyRetrofitCallback cb);
 
     //Request a list of requestable pros for this booking. Example response : {requestable_jobs: [{:name=>"Jason Jones", :id=>2462}, {:name=>"FakeJake Eubank", :id=>2746}]}
     @GET("/bookings/{booking}/request_pro_info")
-    void getRequestProInfo(@Path("booking") int bookingId,
-                           HandyRetrofitCallback cb);
+    void getRequestProInfo(
+            @Path("booking") int bookingId,
+            HandyRetrofitCallback cb
+    );
 
     //Request a specific pro for a specific booking.
     @POST("/bookings/{booking}/request_pro")
-    void requestProForBooking(@Path("booking") int bookingId,
-                              @Query("requested_pro") int requestedProId,
-                              @Query("fail_on_conflict") boolean failOnConflict,
-                              HandyRetrofitCallback cb);
+    void requestProForBooking(
+            @Path("booking") int bookingId,
+            @Query("requested_pro") int requestedProId,
+            @Query("fail_on_conflict") boolean failOnConflict,
+            HandyRetrofitCallback cb
+    );
 
     //Help Center Self Service Center
 
     @GET("/self_service/node_details")
-    void getHelpInfo(@Query("id") String nodeId,
-                     @Query("auth_token") String authToken,
-                     @Query("booking_id") String bookingId,
-                     HandyRetrofitCallback cb);
+    void getHelpInfo(
+            @Query("id") String nodeId,
+            @Query("auth_token") String authToken,
+            @Query("booking_id") String bookingId,
+            HandyRetrofitCallback cb
+    );
 
     @GET("/self_service/booking_node_details")
-    void getHelpBookingsInfo(@Query("id") String nodeId,
-                             @Query("auth_token") String authToken,
-                             @Query("booking_id") String bookingId,
-                             HandyRetrofitCallback cb);
+    void getHelpBookingsInfo(
+            @Query("id") String nodeId,
+            @Query("auth_token") String authToken,
+            @Query("booking_id") String bookingId,
+            HandyRetrofitCallback cb
+    );
 
     @POST("/self_service/create_case")
     void createHelpCase(@Body TypedInput body, HandyRetrofitCallback cb);
