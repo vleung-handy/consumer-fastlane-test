bc211a53fce66e513ae42e6eddef3f118f5ed43e
Ondrej Dolejsi <odolejsi@handy.com>
2016-05-31 16:02:39 -0400

    ProTeam has a fucntional hamburger and a title.

--- app/src/main/java/com/handybook/handybook/module/proteam/ui/fragment/ProTeamFragment.java
+++ app/src/main/java/com/handybook/handybook/module/proteam/ui/fragment/ProTeamFragment.java
@@ -2,13 +2,20 @@ package com.handybook.handybook.module.proteam.ui.fragment;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.handybook.handybook.R;
+import com.handybook.handybook.ui.activity.MenuDrawerActivity;
 import com.handybook.handybook.ui.fragment.InjectedFragment;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +24,13 @@ import com.handybook.handybook.ui.fragment.InjectedFragment;
  */
 public class ProTeamFragment extends InjectedFragment
 {
+    @Bind(R.id.pro_team_toolbar)
+    Toolbar mToolbar;
+    @Bind(R.id.pro_team_tab_layout)
+    TabLayout mTabLayout;
+    @Bind(R.id.pro_team_add_pros)
+    FloatingActionButton mFab;
+
 
     public ProTeamFragment()
     {
@@ -41,7 +55,12 @@ public class ProTeamFragment extends InjectedFragment
             Bundle savedInstanceState
     )
     {
-        return inflater.inflate(R.layout.fragment_pro_team, container, false);
+        final View view = inflater.inflate(R.layout.fragment_pro_team, container, false);
+        ButterKnife.bind(this, view);
+        final MenuDrawerActivity activity = (MenuDrawerActivity) getActivity();
+        activity.setSupportActionBar(mToolbar);
+        activity.setupHamburgerMenu(mToolbar);
+        return view;
     }
 
 }
