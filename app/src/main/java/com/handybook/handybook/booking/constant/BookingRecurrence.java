8863ef1071e3df8423379c62b8e0043f42437a7a
Victoria Leung <vleung@handybook.com>
2016-07-12 17:05:17 -0400

    get booking recurrence options from the server instead

---
+++ app/src/main/java/com/handybook/handybook/booking/constant/BookingRecurrence.java
@@ -0,0 +1,20 @@
+package com.handybook.handybook.booking.constant;
+
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+public class BookingRecurrence //TODO: reorganize package hierarchy so that this is in a better place
+{
+    public final static int ONE_TIME = 0;
+    public final static int WEEKLY = 1;
+    public final static int BIMONTHLY = 2;
+    public final static int MONTHLY = 4;
+
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({ONE_TIME, WEEKLY, BIMONTHLY, MONTHLY})
+    public @interface BookingRecurrenceCode
+    {
+    }
+}
