9b1c651eba857982e6688d0af2e08d3b7d187c25
Victoria Leung <vleung@handybook.com>
2016-09-13 17:30:08 -0400

    remove entry method info local json asset

--- app/src/main/java/com/handybook/handybook/booking/model/EntryMethodsInfo.java
+++ app/src/main/java/com/handybook/handybook/booking/model/EntryMethodsInfo.java
@@ -1,19 +1,9 @@
 package com.handybook.handybook.booking.model;
 
-import android.content.Context;
-import android.support.annotation.NonNull;
-
-import com.crashlytics.android.Crashlytics;
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 import com.google.gson.annotations.SerializedName;
-import com.handybook.handybook.module.configuration.model.Configuration;
-import com.handybook.handybook.util.IOUtils;
 
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
-import java.util.ListIterator;
 
 /**
  * model from server that determines how to display the entry info page to the user
@@ -41,56 +31,6 @@ public class EntryMethodsInfo implements Serializable
     {
         mInstructionText = instructionText;
         mEntryMethodOptions = entryMethodOptions;
-    }
-
-    /**
-     * TODO TEMPORARY HACK, this is getting removed ASAP
-     * @return
-     */
-    public static EntryMethodsInfo getEntryMethodInfo_HACK(Configuration configuration, @NonNull Context context)
-    {
-        String path = "editentry_hack/entry_method_info_hack.json";
-
-        try
-        {
-            String fileContents = IOUtils.loadJSONFromAsset(context, path);
-            try
-            {
-                EntryMethodsInfo entryMethodsInfo =  (new Gson()).fromJson(fileContents, EntryMethodsInfo.class);//TODO: add exception handling
-                boolean lockboxEntryMethodEnabled = configuration != null && configuration.isLockboxEntryMethodEnabled();
-                if(!lockboxEntryMethodEnabled && entryMethodsInfo != null)
-                {
-                    //remove lockbox option if config says no lockbox
-                    List<EntryMethodOption> entryMethodOptions = entryMethodsInfo.getEntryMethodOptions();
-                    ListIterator listIterator = entryMethodOptions.listIterator();
-                    while(listIterator.hasNext())
-                    {
-                        EntryMethodOption entryMethodOption = (EntryMethodOption) listIterator.next();
-                        if(BookingInstruction.InstructionType.EntryMethod.LOCKBOX.equals(
-                                entryMethodOption.getMachineName()))
-                        {
-                            listIterator.remove();
-                            break;
-                        }
-                    }
-                }
-                return entryMethodsInfo;
-
-
-            }
-            catch (JsonSyntaxException ex)
-            {
-                Crashlytics.logException(ex);
-            }
-
-        }
-        catch (IOException e)
-        {
-            Crashlytics.logException(e);
-        }
-
-        //should never be null since we're getting this from the assets folder
-        return null;
     }
 
     public List<EntryMethodOption> getEntryMethodOptions()
