7733b8a5c23813cf75c00dd3043243a168f630e9
odolejsi-handy <odolejsi@handy.com>
2016-11-16 12:26:18 -0500

    Fix a bug, where the wrong price was displayed.

--- app/src/main/java/com/handybook/handybook/booking/ui/fragment/BookingHeaderFragment.java
+++ app/src/main/java/com/handybook/handybook/booking/ui/fragment/BookingHeaderFragment.java
@@ -21,7 +21,8 @@ import java.util.Observer;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public final class BookingHeaderFragment extends BookingFlowFragment implements Observer {
+public final class BookingHeaderFragment extends BookingFlowFragment implements Observer
+{
 
     private static final String TIME_FORMAT = "h:mm aaa";
     private static final String DATE_FORMAT = "EEEE',' MMMM d";
@@ -40,22 +41,27 @@ public final class BookingHeaderFragment extends BookingFlowFragment implements 
     TextView discountText;
 
 
-    static BookingHeaderFragment newInstance() {
+    static BookingHeaderFragment newInstance()
+    {
         return new BookingHeaderFragment();
     }
 
     @Override
-    public final void onCreate(final Bundle savedInstanceState) {
+    public final void onCreate(final Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         transaction = bookingManager.getCurrentTransaction();
         quote = bookingManager.getCurrentQuote();
     }
 
     @Override
-    public final View onCreateView(final LayoutInflater inflater, final ViewGroup container,
-                                   final Bundle savedInstanceState) {
+    public final View onCreateView(
+            final LayoutInflater inflater, final ViewGroup container,
+            final Bundle savedInstanceState
+    )
+    {
         final View view = getActivity().getLayoutInflater()
-                .inflate(R.layout.fragment_booking_header,container, false);
+                                       .inflate(R.layout.fragment_booking_header, container, false);
 
         ButterKnife.bind(this, view);
         discountText.setPaintFlags(discountText.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
@@ -64,7 +70,8 @@ public final class BookingHeaderFragment extends BookingFlowFragment implements 
     }
 
     @Override
-    final public void onStart() {
+    final public void onStart()
+    {
         super.onStart();
         transaction.addObserver(this);
         quote.addObserver(this);
@@ -72,19 +79,22 @@ public final class BookingHeaderFragment extends BookingFlowFragment implements 
     }
 
     @Override
-    final public void onStop() {
+    final public void onStop()
+    {
         super.onStop();
         transaction.deleteObserver(this);
         quote.deleteObserver(this);
     }
 
     @Override
-    public void update(final Observable observable, final Object data) {
+    public void update(final Observable observable, final Object data)
+    {
         if (observable instanceof BookingQuote || observable instanceof BookingTransaction)
-            refreshInfo();
+        { refreshInfo(); }
     }
 
-    private void refreshInfo() {
+    private void refreshInfo()
+    {
         final float hours = transaction.getHours() + transaction.getExtraHours();
         final Date startDate = transaction.getStartDate();
 
@@ -95,29 +105,24 @@ public final class BookingHeaderFragment extends BookingFlowFragment implements 
         }
         else
         {
-            Crashlytics.logException(new RuntimeException("refreshInfo: bookingManager.getCurrentRequest() IS NULL!!!!"));
+            Crashlytics.logException(new RuntimeException(
+                    "refreshInfo: bookingManager.getCurrentRequest() IS NULL!!!!"));
         }
 
         //we want to display the time using the booking location's time zone
         dateText.setText(DateTimeUtils.formatDate(startDate, DATE_FORMAT, timeZone));
 
-        timeText.setText(DateTimeUtils.formatDate(startDate, TIME_FORMAT, timeZone) + " - "
-                + TextUtils.formatDecimal(hours, DECIMAL_FORMAT)
-                + " " + getString(R.string.hours));
+        timeText.setText(DateTimeUtils.formatDate(startDate, TIME_FORMAT, timeZone)
+                                 + " - "
+                                 + TextUtils.formatDecimal(hours, DECIMAL_FORMAT)
+                                 + " " + getString(R.string.hours)
+        );
 
         final float[] pricing = quote.getPricing(hours, transaction.getRecurringFrequency());
         final String currChar = quote.getCurrencyChar();
 
-        if (pricing[1] < pricing[0])
-        {
-            priceText.setText(TextUtils.formatPrice(pricing[1], currChar, null));
-            discountText.setText(TextUtils.formatPrice(pricing[0], currChar, null));
-            discountText.setVisibility(View.VISIBLE);
-        }
-        else
-        {
-            priceText.setText(TextUtils.formatPrice(pricing[0], currChar, null));
-            discountText.setVisibility(View.GONE);
-        }
+        priceText.setText(TextUtils.formatPrice(pricing[1], currChar, null));
+        discountText.setText(TextUtils.formatPrice(pricing[0], currChar, null));
+        discountText.setVisibility(pricing[1] < pricing[0] ? View.VISIBLE : View.GONE);
     }
 }
