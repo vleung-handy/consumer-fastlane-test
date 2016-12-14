2ce2c34a89fb27ef59fd92c5aad03feb8e154a93
odolejsi-handy <odolejsi@handy.com>
2016-12-14 14:28:22 -0500

    Slightly inproved conversion of new commitment procing table to the old one, because handymen services don't return recurrence options.

--- app/src/main/java/com/handybook/handybook/booking/model/CommitmentPricesMap.java
+++ app/src/main/java/com/handybook/handybook/booking/model/CommitmentPricesMap.java
@@ -14,50 +14,24 @@ public class CommitmentPricesMap extends HashMap<String, CommitmentPricesMap.Com
                 .get("0")
                 .getFrequencyHashMap();
         ArrayList<String> keys = new ArrayList<>(crf.get("price").getPriceItemHashMap().keySet());
-        for (String hour_key : keys)
+        for (String hoursKey : keys)
         {
 
-            final float hours = Float.valueOf(hour_key);
-            final float price = crf
-                    .get("price")
-                    .getPriceItemHashMap()
-                    .get(hour_key)
-                    .getFullPrice() / 100.0f;
-            final float discountPrice = crf
-                    .get("price")
-                    .getPriceItemHashMap()
-                    .get(hour_key)
-                    .getAmountDue() / 100.0f;
-            final float biMonthlyPrice = crf
-                    .get("bimonthly_recurring_price")
-                    .getPriceItemHashMap()
-                    .get(hour_key)
-                    .getFullPrice() / 100.0f;
-            final float discountBiMonthlyPrice = crf
-                    .get("bimonthly_recurring_price")
-                    .getPriceItemHashMap()
-                    .get(hour_key)
-                    .getAmountDue() / 100.0f;
-            final float monthlyPrice = crf
-                    .get("monthly_recurring_price")
-                    .getPriceItemHashMap()
-                    .get(hour_key)
-                    .getFullPrice() / 100.0f;
-            final float discountMonthlyPrice = crf
-                    .get("monthly_recurring_price")
-                    .getPriceItemHashMap()
-                    .get(hour_key)
-                    .getAmountDue() / 100.0f;
-            final float weeklyPrice = crf
-                    .get("weekly_recurring_price")
-                    .getPriceItemHashMap()
-                    .get(hour_key)
-                    .getFullPrice() / 100.0f;
-            final float discountWeeklyPrice = crf
-                    .get("weekly_recurring_price")
-                    .getPriceItemHashMap()
-                    .get(hour_key)
-                    .getAmountDue() / 100.0f;
+            final float hours = Float.valueOf(hoursKey);
+            final float price = getPrice("price", hoursKey);
+            final float discountPrice = getDiscountPrice("price", hoursKey);
+            final float biMonthlyPrice = getPrice("bimonthly_recurring_price", hoursKey);
+            final float discountBiMonthlyPrice = getDiscountPrice(
+                    "bimonthly_recurring_price",
+                    hoursKey
+            );
+            final float monthlyPrice = getPrice("monthly_recurring_price", hoursKey);
+            final float discountMonthlyPrice = getDiscountPrice(
+                    "monthly_recurring_price",
+                    hoursKey
+            );
+            final float weeklyPrice = getPrice("weekly_recurring_price", hoursKey);
+            final float discountWeeklyPrice = getDiscountPrice("weekly_recurring_price", hoursKey);
             priceTable.add(new BookingPriceInfo(
                     hours,
                     price,
@@ -71,6 +45,53 @@ public class CommitmentPricesMap extends HashMap<String, CommitmentPricesMap.Com
             ));
         }
         return priceTable;
+    }
+
+    private float getPrice(final String recurrenceKey, final String hourKey)
+    {
+        float fullPriceDollars;
+        try
+        {
+            final HashMap<String, CommitmentRecurrenceFrequency> crf = get("no_commitment")
+                    .get("0")
+                    .getFrequencyHashMap();
+            final int fullPriceCents = crf
+                    .get(recurrenceKey)
+                    .getPriceItemHashMap()
+                    .get(hourKey)
+                    .getFullPrice();
+            fullPriceDollars = fullPriceCents / 100f;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            fullPriceDollars = 0f;
+        }
+        return fullPriceDollars;
+    }
+
+    private float getDiscountPrice(final String recurrenceKey, final String hourKey)
+    {
+        float fullPriceDollars;
+        try
+        {
+            final HashMap<String, CommitmentRecurrenceFrequency> crf = get("no_commitment")
+                    .get("0")
+                    .getFrequencyHashMap();
+            final int fullPriceCents = crf
+                    .get(recurrenceKey)
+                    .getPriceItemHashMap()
+                    .get(hourKey)
+                    .getAmountDue();
+            fullPriceDollars = fullPriceCents / 100f;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            fullPriceDollars = 0f;
+
+        }
+        return fullPriceDollars;
     }
 
     public static class CommitmentType extends HashMap<String, CommitmentLength>
