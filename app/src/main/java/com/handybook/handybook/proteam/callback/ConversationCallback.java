9d6138ff4361404af860470790807f225f162094
Roumwelle Sta Ines <rstaines@handy.com>
2017-02-01 15:43:23 -0500

    Refactor callback logic for creating conversations into its own set of classes for reusability

---
+++ app/src/main/java/com/handybook/handybook/proteam/callback/ConversationCallback.java
@@ -0,0 +1,8 @@
+package com.handybook.handybook.proteam.callback;
+
+public interface ConversationCallback
+{
+    void onCreateConversationSuccess(String conversationId);
+
+    void onCreateConversationError();
+}
---
+++ app/src/main/java/com/handybook/handybook/proteam/callback/ConversationCallbackWrapper.java
@@ -0,0 +1,47 @@
+package com.handybook.handybook.proteam.callback;
+
+import com.handybook.shared.layer.model.CreateConversationResponse;
+
+import java.lang.ref.WeakReference;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class ConversationCallbackWrapper implements Callback<CreateConversationResponse>
+{
+    private WeakReference<ConversationCallback> mCallback;
+
+    public ConversationCallbackWrapper(ConversationCallback callback)
+    {
+        mCallback = new WeakReference<>(callback);
+    }
+
+    @Override
+    public void success(
+            final CreateConversationResponse createConversationResponse, final Response response
+    )
+    {
+        if (mCallback.get() != null)
+        {
+            if (createConversationResponse.isSuccess())
+            {
+                mCallback.get().onCreateConversationSuccess(
+                        createConversationResponse.getConversationId());
+            }
+            else
+            {
+                mCallback.get().onCreateConversationError();
+            }
+        }
+    }
+
+    @Override
+    public void failure(final RetrofitError error)
+    {
+        if (mCallback.get() != null)
+        {
+            mCallback.get().onCreateConversationError();
+        }
+    }
+}
