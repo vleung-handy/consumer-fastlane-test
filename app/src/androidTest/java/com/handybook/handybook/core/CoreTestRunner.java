0abe344fe1e0a8f419e55768ce806b3d744d27ab
Xi Wei <xwei@handy.com>
2017-03-02 14:35:13 -0500

    CAND-1155 inject config params instead of using the ones from server

---
+++ app/src/androidTest/java/com/handybook/handybook/core/CoreTestRunner.java
@@ -0,0 +1,14 @@
+package com.handybook.handybook.core;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.test.runner.AndroidJUnitRunner;
+
+public class CoreTestRunner extends AndroidJUnitRunner {
+
+    @Override
+    public Application newApplication(ClassLoader cl, String className, Context context)
+            throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+        return super.newApplication(cl, CoreTestApplication.class.getName(), context);
+    }
+}
