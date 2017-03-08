03e48c11c4fa95407c11f78cba6c5db5f4345f8a
Xi Wei <xwei@handy.com>
2017-03-08 11:50:00 -0500

    CAND-1160 created chat options for booking.

---
+++ app/src/main/java/com/handybook/handybook/booking/model/ChatOptions.java
@@ -0,0 +1,21 @@
+package com.handybook.handybook.booking.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class ChatOptions implements Serializable {
+
+    @SerializedName("allow_chat")
+    private boolean mAllowChat;
+    @SerializedName("direct_to_in_app_chat")
+    private boolean mDirectToInAppChat;
+
+    public boolean isAllowChat() {
+        return mAllowChat;
+    }
+
+    public boolean isDirectToInAppChat() {
+        return mDirectToInAppChat;
+    }
+}
