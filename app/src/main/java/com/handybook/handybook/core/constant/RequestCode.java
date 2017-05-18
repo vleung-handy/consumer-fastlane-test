e3a7110143e1dac97e70ba4da03613bb1370fe2a
odolejsi-handy <odolejsi@handy.com>
2017-05-18 16:58:04 -0400

    java.lang.IllegalArgumentException: Can only use lower 16 bits for requestCode

--- app/src/main/java/com/handybook/handybook/core/constant/RequestCode.java
+++ app/src/main/java/com/handybook/handybook/core/constant/RequestCode.java
@@ -2,11 +2,11 @@ package com.handybook.handybook.core.constant;
 
 public class RequestCode {
 
-    public static final int EDIT_PRO_TEAM = 100100;
-    public static final int EDIT_PRO_TEAM_PREFERENCE = 100110;
-    public static final int EDIT_PLAN_ADDRESS = 100200;
-    public static final int EDIT_PLAN_FREQUENCY = 100210;
-    public static final int EDIT_PLAN_HOURS = 100220;
-    public static final int EDIT_PLAN_EXTRAS = 100230;
-    public static final int LOGIN_FROM_ONBOARDING = 100300;
+    public static final int EDIT_PRO_TEAM = 10100;
+    public static final int EDIT_PRO_TEAM_PREFERENCE = 10110;
+    public static final int EDIT_PLAN_ADDRESS = 10200;
+    public static final int EDIT_PLAN_FREQUENCY = 10210;
+    public static final int EDIT_PLAN_HOURS = 10220;
+    public static final int EDIT_PLAN_EXTRAS = 10230;
+    public static final int LOGIN_FROM_ONBOARDING = 10300;
 }
