44631b625b6a854d86fabbd13721d32a252cda07
odolejsi-handy <odolejsi@handy.com>
2017-06-08 15:00:18 -0400

    Conetti stab

--- app/src/main/java/com/handybook/handybook/vegas/ui/RatingFlowGameFragment.java
+++ app/src/main/java/com/handybook/handybook/vegas/ui/RatingFlowGameFragment.java
@@ -12,6 +12,7 @@ import com.handybook.handybook.library.ui.fragment.InjectedFragment;
 import com.handybook.handybook.vegas.model.GameResponse;
 import com.handybook.handybook.vegas.ui.view.ScratchView;
 import com.handybook.handybook.vegas.ui.view.ScratchableInterface;
+import com.plattysoft.leonids.ParticleSystem;
 
 import java.util.Locale;
 
@@ -27,9 +28,29 @@ public class RatingFlowGameFragment extends InjectedFragment {
 
     private String mGame;
     private double mRevealedPercentage = 0;
+    private int[] mParticleIds = {
+            R.drawable.confetti_1,
+            R.drawable.confetti_2,
+            R.drawable.confetti_3,
+            R.drawable.confetti_4,
+            R.drawable.confetti_5,
+            R.drawable.confetti_6,
+            R.drawable.confetti_7,
+            R.drawable.confetti_8,
+            R.drawable.confetti_9,
+            R.drawable.confetti_10,
+            R.drawable.confetti_11,
+            R.drawable.confetti_12
+
+    };
 
     @Bind(R.id.rfgf_percentage) TextView mPercentage;
     @Bind(R.id.rfgf_scratch_view) ScratchView mScratchView;
+    @Bind(R.id.particle_emitter_a) View mEmitterA;
+    @Bind(R.id.particle_emitter_b) View mEmitterB;
+    @Bind(R.id.particle_emitter_c) View mEmitterC;
+    @Bind(R.id.particle_emitter_d) View mEmitterD;
+    @Bind(R.id.particle_emitter_e) View mEmitterE;
 
     public RatingFlowGameFragment() {
     }
@@ -92,4 +113,36 @@ public class RatingFlowGameFragment extends InjectedFragment {
         updatePercentage(0f);
     }
 
+    @OnClick(R.id.rfgf_blast_button)
+    void blastConfetti() {
+        int partsPerSec = 4;
+        int emitTime = 2000;
+        int maxParts = 400;
+        int timeToLive = 10000;
+        for (final int drawableId : mParticleIds) {
+            emitFromView(mEmitterA, partsPerSec, emitTime, maxParts, timeToLive, drawableId);
+            emitFromView(mEmitterB, partsPerSec, emitTime, maxParts, timeToLive, drawableId);
+            emitFromView(mEmitterC, partsPerSec, emitTime, maxParts, timeToLive, drawableId);
+            emitFromView(mEmitterD, partsPerSec, emitTime, maxParts, timeToLive, drawableId);
+            emitFromView(mEmitterE, partsPerSec, emitTime, maxParts, timeToLive, drawableId);
+        }
+    }
+
+    private void emitFromView(
+            final View sourceView,
+            final int partNumPerSecond,
+            final int emitTime,
+            final int maxParticles,
+            final int timeToLive,
+            final int drawableId
+    ) {
+        new ParticleSystem(getActivity(), maxParticles, drawableId, timeToLive)
+                .setSpeedModuleAndAngleRange(0.1f, 0.3f, 225, 315)
+                .setRotationSpeed(144)
+                .setAcceleration(0.000685f, 90)
+                .setScaleRange(0.3f, 0.5f)
+                .emit(sourceView, partNumPerSecond, emitTime);
+    }
+
 }
+
