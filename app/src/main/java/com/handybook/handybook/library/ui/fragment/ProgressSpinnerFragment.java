f09223adddab4497b3bdb87ff6290a1eaf2d6456
Sammy Ng <sng@handy.com>
2017-06-12 17:12:50 -0400

    Added comments

--- app/src/main/java/com/handybook/handybook/library/ui/fragment/ProgressSpinnerFragment.java
+++ app/src/main/java/com/handybook/handybook/library/ui/fragment/ProgressSpinnerFragment.java
@@ -15,6 +15,9 @@ public abstract class ProgressSpinnerFragment extends InjectedFragment {
     private View mOverlay;
 
     @Override
+    /**
+     * This should be overridden if you want to display the Progress Spinner
+     */
     public View onCreateView(
             final LayoutInflater inflater,
             @Nullable final ViewGroup container,
@@ -41,6 +44,8 @@ public abstract class ProgressSpinnerFragment extends InjectedFragment {
 
     protected void hideProgressSpinner() {
         //If this is null, it means it wasn't initialized, so ignore
+        //Show progress spinner was left alone, because if we want to show it, let it crash to know
+        // to override onCreateView
         if(mProgressSpinner == null) {
             return;
         }
